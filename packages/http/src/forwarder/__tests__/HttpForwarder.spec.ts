8c24c4ddd1bc0c5405f03500947179ceec009d18
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-04-02 14:12:17 +0200

    test: fix http forwarder tests

--- packages/http/src/forwarder/__tests__/HttpForwarder.spec.ts
+++ packages/http/src/forwarder/__tests__/HttpForwarder.spec.ts
@@ -7,20 +7,21 @@ describe('HttpForwarder', () => {
   const forwarder = new HttpForwarder();
 
   beforeEach(() => {
-    jest.spyOn(axios, 'default').mockImplementation(jest.fn);
+    jest.spyOn(axios, 'default');
   });
 
   describe('forward()', () => {
     describe("parameters haven' been provided", () => {
       it('proxies request correctly', async () => {
-        jest.spyOn(axios, 'default').mockImplementation(() => ({
+        jest.spyOn(axios, 'default').mockResolvedValue({
           status: 200,
           headers: {
             'Content-type': 'application/json',
           },
           data: '[{},{}]',
           statusText: 'ok',
-        }));
+          config: {}
+        });
 
         const request = Object.assign({}, httpRequests[0]);
         request.data.url.baseUrl = 'http://api.example.com';
@@ -40,14 +41,15 @@ describe('HttpForwarder', () => {
     describe('parameters are valid', () => {
       describe('server url has no variables', () => {
         it('proxies request correctly', async () => {
-          jest.spyOn(axios, 'default').mockImplementation(() => ({
+          jest.spyOn(axios, 'default').mockResolvedValue({
             status: 200,
             headers: {
               'Content-type': 'application/json',
             },
             data: '[{},{}]',
             statusText: 'ok',
-          }));
+            config: {}
+          });
 
           const response = await forwarder.forward({
             input: httpRequests[0],
