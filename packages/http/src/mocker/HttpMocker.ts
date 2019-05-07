a1c5d2a5a0b1df0c70e4413f933eb5ea3f23e27a
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-07 13:15:18 +0200

    refactor: no any

--- packages/http/src/mocker/HttpMocker.ts
+++ packages/http/src/mocker/HttpMocker.ts
@@ -9,7 +9,7 @@ import helpers from './negotiator/NegotiatorHelpers';
 
 export class HttpMocker
   implements IMocker<IHttpOperation, IHttpRequest, IHttpConfig, IHttpResponse> {
-  constructor(private _exampleGenerator: IExampleGenerator<any>) { }
+  constructor(private _exampleGenerator: IExampleGenerator) { }
 
   public async mock({
     resource,
