5a0f3cada81f4f21b0082e025ef113e2e5741785
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-07 12:38:43 +0200

    feat: replace 400 error for missing invalid response with problem/json

---
+++ packages/http/src/mocker/errors.ts
@@ -0,0 +1,5 @@
+export const MISSING_INVALID_RESPONSE_TEMPLATE = {
+  name: 'MISSING_INVALID_RESPONSE_TEMPLATE',
+  title: 'Missing invalid response or response example for the current invalid request',
+  status: 400,
+}
