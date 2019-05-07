84b284bac021d5ecbca6b3cf75c80b6e9986f70c
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-07 18:24:31 +0200

    fix: correct status code is 422

--- packages/http/src/mocker/errors.ts
+++ packages/http/src/mocker/errors.ts
@@ -4,5 +4,5 @@ import { ProblemJson } from '../types';
 export const INVALID_REQUEST_PAYLOAD: Omit<ProblemJson, 'detail'> = {
   name: 'INVALID_REQUEST_PAYLOAD',
   title: 'Invalid request body payload',
-  status: 412,
+  status: 422,
 };
