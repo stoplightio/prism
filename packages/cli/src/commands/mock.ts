76501424890e0f7a72d82c569210e22c4e85a17b
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-13 08:16:51 -0500

    feat: CLI show endpoints and status

--- packages/cli/src/commands/mock.ts
+++ packages/cli/src/commands/mock.ts
@@ -1,4 +1,5 @@
 import { Command } from '@oclif/command';
+import * as signale from 'signale';
 import { ARGS, FLAGS } from '../const/options';
 import { createServer } from '../util/createServer';
 
@@ -8,15 +9,27 @@ export default class Server extends Command {
   public static args = [ARGS.spec];
 
   public async run() {
+    const i = new signale.Signale({ interactive: true });
+
+    i.await('Starting Prism…');
+
     const {
       flags: { port },
       args: { spec },
     } = this.parse(Server);
 
     const server = createServer(spec, { mock: true });
-
     const address = await server.listen(port);
 
-    this.log(address);
+    if (server.prism.resources.length === 0) {
+      i.fatal('No operations found in the current file.');
+      this.exit(1);
+    }
+
+    i.success(`Prism is listening on ${address}`);
+
+    server.prism.resources.forEach(resource => {
+      signale.note(`${resource.method.toUpperCase().padEnd(10)} ${address}${resource.path}`);
+    });
   }
 }
