edfc585c8570b4d170c6036ebd58e032688e17a7
Mateusz Wit <mateusz.wit@travelport.com>
2019-05-20 18:25:51 +0200

    Test circle ci run 3

--- jest.config.js
+++ jest.config.js
@@ -25,46 +25,46 @@ module.exports = {
       displayName: 'HARNESS',
       testMatch: ['<rootDir>/test-harness/index.js']
     },
-    {
-      ...projectDefault,
-      displayName: 'HTTP-SERVER',
-      testMatch: ['<rootDir>/packages/http-server/src/**/__tests__/*.(spec|unit|int|func).(ts)?(x)'],
-      globals: {
-        'ts-jest': {
-          tsConfig: '<rootDir>/packages/http-server/tsconfig.json',
-        },
-      },
-    },
-    {
-      ...projectDefault,
-      displayName: 'HTTP',
-      testMatch: ['<rootDir>/packages/http/src/**/__tests__/*.(spec|unit|int|func).(ts)?(x)'],
-      globals: {
-        'ts-jest': {
-          tsConfig: '<rootDir>/packages/http/tsconfig.json',
-        },
-      },
-    },
-    {
-      ...projectDefault,
-      displayName: 'CORE',
-      testMatch: ['<rootDir>/packages/core/src/**/__tests__/*.(spec|unit|int|func).(ts)?(x)'],
-      globals: {
-        'ts-jest': {
-          tsConfig: '<rootDir>/packages/core/tsconfig.json',
-        },
-      },
-    },
-    {
-      ...projectDefault,
-      displayName: 'CLI',
-      testMatch: ['<rootDir>/packages/cli/src/**/__tests__/*.(spec|unit|int|func).(ts)?(x)'],
-      globals: {
-        'ts-jest': {
-          tsConfig: '<rootDir>/packages/cli/tsconfig.json',
-        },
-      },
-    },
+    // {
+    //   ...projectDefault,
+    //   displayName: 'HTTP-SERVER',
+    //   testMatch: ['<rootDir>/packages/http-server/src/**/__tests__/*.(spec|unit|int|func).(ts)?(x)'],
+    //   globals: {
+    //     'ts-jest': {
+    //       tsConfig: '<rootDir>/packages/http-server/tsconfig.json',
+    //     },
+    //   },
+    // },
+    // {
+    //   ...projectDefault,
+    //   displayName: 'HTTP',
+    //   testMatch: ['<rootDir>/packages/http/src/**/__tests__/*.(spec|unit|int|func).(ts)?(x)'],
+    //   globals: {
+    //     'ts-jest': {
+    //       tsConfig: '<rootDir>/packages/http/tsconfig.json',
+    //     },
+    //   },
+    // },
+    // {
+    //   ...projectDefault,
+    //   displayName: 'CORE',
+    //   testMatch: ['<rootDir>/packages/core/src/**/__tests__/*.(spec|unit|int|func).(ts)?(x)'],
+    //   globals: {
+    //     'ts-jest': {
+    //       tsConfig: '<rootDir>/packages/core/tsconfig.json',
+    //     },
+    //   },
+    // },
+    // {
+    //   ...projectDefault,
+    //   displayName: 'CLI',
+    //   testMatch: ['<rootDir>/packages/cli/src/**/__tests__/*.(spec|unit|int|func).(ts)?(x)'],
+    //   globals: {
+    //     'ts-jest': {
+    //       tsConfig: '<rootDir>/packages/cli/tsconfig.json',
+    //     },
+    //   },
+    // },
   ],
   collectCoverageFrom: [
     '**/src/**/*.{ts,tsx}',
