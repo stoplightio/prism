46dfe0f9ef06a87e251c8a0fbd9db7a80f7f69e8
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-27 13:01:56 +0200

    feat: add main logging logic to core

---
+++ packages/core/src/logger.ts
@@ -0,0 +1,23 @@
+import * as pino from 'pino';
+import { levels } from 'pino';
+
+levels.labels[10] = 'note';
+levels.values.note = 10;
+levels.labels[20] = 'success';
+levels.values.success = 20;
+
+function createLogger(name: string) {
+  const options: pino.LoggerOptions = {
+    name,
+    customLevels: {
+      note: 10,
+    },
+    level: 'note',
+    base: null,
+    timestamp: false,
+  };
+
+  return pino(options);
+}
+
+export { levels as logLevels, createLogger };
