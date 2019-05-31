8d1a3baa1dd175a418066c7e5ee05213c94f122b
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-05-31 18:58:02 +0200

    fix: always set JSON to serialise

--- packages/http-server/src/server.ts
+++ packages/http-server/src/server.ts
@@ -1,7 +1,7 @@
 import { configMergerFactory } from '@stoplight/prism-core';
 import { createInstance, IHttpMethod, ProblemJsonError, TPrismHttpInstance } from '@stoplight/prism-http';
 import * as fastify from 'fastify';
-import { IncomingMessage, Server, ServerResponse } from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 import { getHttpConfigFromRequest } from './getHttpConfigFromRequest';
 import { IPrismHttpServer, IPrismHttpServerOpts } from './types';
 
@@ -9,7 +9,7 @@ export const createServer = <LoaderInput>(
   loaderInput: LoaderInput,
   opts: IPrismHttpServerOpts<LoaderInput>,
 ): IPrismHttpServer<LoaderInput> => {
-  const server = fastify<Server, IncomingMessage, ServerResponse>();
+  const server = fastify();
   const { components = {}, config } = opts;
   const mergedConfig = configMergerFactory({ mock: { dynamic: false } }, config, getHttpConfigFromRequest);
 
@@ -71,7 +71,7 @@ const replyHandler = <LoaderInput>(
           reply.headers(output.headers);
         }
 
-        reply.send(output.body);
+        reply.serializer(JSON.stringify).send(output.body);
       } else {
         throw new Error('Unable to find any decent response for the current request.');
       }
