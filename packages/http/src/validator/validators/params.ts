f8e7d07e2bfa81b54753bb728de12d12b4a7e5b8
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-06-04 16:09:05 +0200

    refactor: create a JSON Schema out of the parameters

--- packages/http/src/validator/validators/params.ts
+++ packages/http/src/validator/validators/params.ts
@@ -1,7 +1,8 @@
 import { DiagnosticSeverity, HttpParamStyles, IHttpParam } from '@stoplight/types';
-import { upperFirst } from 'lodash';
+import { compact, keyBy, mapValues, pickBy, upperFirst } from 'lodash';
 
 import { IPrismDiagnostic } from '@stoplight/prism-core/src/types';
+import { JSONSchema4 } from 'json-schema';
 import { IHttpParamDeserializerRegistry } from '../deserializers/types';
 import { IHttpValidator } from './types';
 import { validateAgainstSchema } from './utils';
@@ -15,45 +16,37 @@ export class HttpParamsValidator<Target, Spec extends IHttpParam> implements IHt
 
   public validate(target: Target, specs: Spec[]): IPrismDiagnostic[] {
     const { _registry: registry, _prefix: prefix, _style: style } = this;
-    return specs.reduce<IPrismDiagnostic[]>((results, spec) => {
-      if (!(spec.name in target) && spec.required === true) {
-        results.push({
-          path: [prefix, spec.name],
-          code: 'required',
-          message: `Missing ${spec.name} ${prefix} param`,
-          severity: DiagnosticSeverity.Error,
-        });
 
-        // stop further checks
-        return results;
-      }
+    const schema = createJsonSchemaFromParams(specs);
 
-      const resolvedStyle = spec.style || style;
-      if (spec.content && spec.content.schema && target[spec.name]) {
+    const deprecatedWarnings = specs.filter(spec => spec.deprecated).map(spec => ({
+      path: [prefix, spec.name],
+      code: 'deprecated',
+      message: `${upperFirst(prefix)} param ${spec.name} is deprecated`,
+      severity: DiagnosticSeverity.Warning,
+    }));
+
+    const parameterValues = pickBy(
+      mapValues(keyBy(specs, 'name'), el => {
+        const resolvedStyle = el.style || style;
         const deserializer = registry.get(resolvedStyle);
 
-        if (deserializer) {
-          Array.prototype.push.apply(
-            results,
-            validateAgainstSchema(
-              deserializer.deserialize(spec.name, target, spec.content.schema, spec.explode || false),
-              spec.content.schema,
-              prefix,
-            ),
-          );
-        }
-      }
+        return deserializer!.deserialize(el.name, target, schema.properties![el.name], el.explode || false);
+      }),
+    );
 
-      if (spec.deprecated === true) {
-        results.push({
-          path: [prefix, spec.name],
-          code: 'deprecated',
-          message: `${upperFirst(prefix)} param ${spec.name} is deprecated`,
-          severity: DiagnosticSeverity.Warning,
-        });
-      }
+    const schemaErrors = validateAgainstSchema(parameterValues, schema, prefix);
 
-      return results;
-    }, []);
+    return schemaErrors.concat(deprecatedWarnings);
   }
+}
+
+function createJsonSchemaFromParams(params: IHttpParam[]): JSONSchema4 {
+  const schema: JSONSchema4 = {
+    type: 'object',
+    properties: pickBy(mapValues(keyBy(params, 'name'), 'content.schema')),
+    required: compact(params.map(m => (m.required ? m.name : undefined))),
+  };
+
+  return schema;
 }
