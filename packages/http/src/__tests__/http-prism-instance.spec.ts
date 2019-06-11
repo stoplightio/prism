e1679fa8642b1ddbff0a823ef8837b776f1818a5
Chris Miaskowski <chrism@stoplight.io>
2019-06-11 16:04:39 +0200

    fix: a bug when we incorrectly matched a path
    SO-257

--- packages/http/src/__tests__/http-prism-instance.spec.ts
+++ packages/http/src/__tests__/http-prism-instance.spec.ts
@@ -4,109 +4,143 @@ import { relative, resolve } from 'path';
 import { createInstance, IHttpConfig, IHttpRequest, IHttpResponse, ProblemJsonError } from '../';
 import { forwarder } from '../forwarder';
 import { UNPROCESSABLE_ENTITY } from '../mocker/errors';
-import { NO_PATH_MATCHED_ERROR } from '../router/errors';
+import { NO_PATH_MATCHED_ERROR, NO_SERVER_MATCHED_ERROR } from '../router/errors';
 
 const fixturePath = (filename: string) => relative(process.cwd(), resolve(__dirname, 'fixtures', filename));
 const noRefsPetstoreMinimalOas2Path = fixturePath('no-refs-petstore-minimal.oas2.json');
 const petStoreOas2Path = fixturePath('petstore.oas2.yaml');
 const staticExamplesOas2Path = fixturePath('static-examples.oas2.json');
+const serverValidationOas2Path = fixturePath('server-validation.oas2.json');
+const serverValidationOas3Path = fixturePath('server-validation.oas3.json');
 
 describe('Http Prism Instance function tests', () => {
   let prism: IPrism<IHttpOperation, IHttpRequest, IHttpResponse, IHttpConfig, { path: string }>;
 
-  beforeAll(async () => {
-    prism = createInstance({ mock: { dynamic: false } }, {});
-    await prism.load({ path: noRefsPetstoreMinimalOas2Path });
-  });
-
-  test('keeps the instances separate', async () => {
-    const secondPrism = createInstance({ mock: { dynamic: false } }, {});
-    await secondPrism.load({ path: noRefsPetstoreMinimalOas2Path });
-
-    expect(prism.resources).toStrictEqual(secondPrism.resources);
-  });
-
-  test('when processing unknown path throws error', () => {
-    return expect(
-      prism.process({
-        method: 'get',
-        url: {
-          path: '/unknown-path',
-        },
-      }),
-    ).rejects.toThrowError(ProblemJsonError.fromTemplate(NO_PATH_MATCHED_ERROR));
-  });
-
-  describe('when processing GET /pet', () => {
-    // TODO will be fixed by https://stoplightio.atlassian.net/browse/SO-260
-    xtest('without an optional body parameter expect 200 response', async () => {
-      const response = await prism.process({
-        method: 'get',
-        url: { path: '/pet' },
-      });
-
-      expect(response.output!.statusCode).toEqual(200);
-    });
-  });
-
-  describe('when processing GET /pet/findByStatus', () => {
-    test('with valid query params returns generated body', async () => {
-      const response = await prism.process({
-        method: 'get',
-        url: {
-          path: '/pet/findByStatus',
-          query: {
-            status: ['available', 'pending'],
-          },
-        },
-      });
-
-      const parsedBody = response!.output!.body;
-
-      expect(typeof parsedBody).toBe('string');
-      expect(response).toMatchSnapshot({
-        output: {
-          body: expect.anything(),
-        },
-      });
+  describe.each([[serverValidationOas2Path], [serverValidationOas3Path]])('given %s', oasFilePath => {
+    beforeAll(async () => {
+      prism = createInstance({ mock: { dynamic: false } }, {});
+      await prism.load({ path: oasFilePath });
     });
 
-    test('w/o required params throws a validation error', () => {
+    test('when processed w/o baseUrl ignore server validation', async () => {
+      const result = await prism.process({
+        method: 'get',
+        url: {
+          path: '/pet',
+        },
+      });
+
+      expect(result.output!.statusCode).toBe(200);
+    });
+
+    test('when processed with valid baseUrl should return 200', async () => {
+      const result = await prism.process({
+        method: 'get',
+        url: {
+          path: '/pet',
+          baseUrl: 'http://example.com/api',
+        },
+      });
+
+      expect(result.output!.statusCode).toBe(200);
+    });
+
+    test('when processed with invalid host should throw error', () => {
       return expect(
         prism.process({
           method: 'get',
           url: {
-            path: '/pet/findByStatus',
+            path: '/pet',
+            baseUrl: 'http://acme.com/api',
           },
         }),
-      ).rejects.toThrowError(ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY));
+      ).rejects.toThrowError(ProblemJsonError.fromTemplate(NO_SERVER_MATCHED_ERROR));
     });
 
-    test('with valid body param then returns no validation issues', async () => {
-      const response = await prism.process({
-        method: 'get',
-        url: {
-          path: '/pet/findByStatus',
-          query: {
-            status: ['available'],
-          },
-        },
-        body: {
-          id: 1,
-          status: 'placed',
-          complete: true,
-        },
-      });
-      expect(response.validations).toEqual({
-        input: [],
-        output: [],
-      });
-    });
-
-    // TODO: will be fixed by https://stoplightio.atlassian.net/browse/SO-259
-    xtest('with invalid body returns validation errors', () => {
+    test('when processed with invalid basePath should throw error', () => {
       return expect(
         prism.process({
+          method: 'get',
+          url: {
+            path: '/pet',
+            baseUrl: 'http://example.com/v1',
+          },
+        }),
+      ).rejects.toThrowError(ProblemJsonError.fromTemplate(NO_SERVER_MATCHED_ERROR));
+    });
+  });
+
+  describe('given no-refs-petstore-minimal.oas2.json', () => {
+    beforeAll(async () => {
+      prism = createInstance({ mock: { dynamic: false } }, {});
+      await prism.load({ path: noRefsPetstoreMinimalOas2Path });
+    });
+
+    test('keeps the instances separate', async () => {
+      const secondPrism = createInstance({ mock: { dynamic: false } }, {});
+      await secondPrism.load({ path: noRefsPetstoreMinimalOas2Path });
+
+      expect(prism.resources).toStrictEqual(secondPrism.resources);
+    });
+
+    test('when processing unknown path throws error', () => {
+      return expect(
+        prism.process({
+          method: 'get',
+          url: {
+            path: '/unknown-path',
+          },
+        }),
+      ).rejects.toThrowError(ProblemJsonError.fromTemplate(NO_PATH_MATCHED_ERROR));
+    });
+
+    describe('when processing GET /pet', () => {
+      // TODO will be fixed by https://stoplightio.atlassian.net/browse/SO-260
+      xtest('without an optional body parameter expect 200 response', async () => {
+        const response = await prism.process({
+          method: 'get',
+          url: { path: '/pet' },
+        });
+
+        expect(response.output!.statusCode).toEqual(200);
+      });
+    });
+
+    describe('when processing GET /pet/findByStatus', () => {
+      test('with valid query params returns generated body', async () => {
+        const response = await prism.process({
+          method: 'get',
+          url: {
+            path: '/pet/findByStatus',
+            query: {
+              status: ['available', 'pending'],
+            },
+          },
+        });
+
+        const parsedBody = response!.output!.body;
+
+        expect(typeof parsedBody).toBe('string');
+        expect(response).toMatchSnapshot({
+          output: {
+            body: expect.anything(),
+          },
+        });
+      });
+
+      test('w/o required params throws a validation error', () => {
+        return expect(
+          prism.process({
+            method: 'get',
+            url: {
+              path: '/pet/findByStatus',
+            },
+          }),
+        ).rejects.toThrowError(ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY));
+      });
+
+      test('with valid body param then returns no validation issues', async () => {
+        const response = await prism.process({
           method: 'get',
           url: {
             path: '/pet/findByStatus',
@@ -115,12 +149,36 @@ describe('Http Prism Instance function tests', () => {
             },
           },
           body: {
-            id: 'should not be a string',
-            status: 'should be one of "placed", "approved", "delivered"',
-            complete: 'should be a boolean',
+            id: 1,
+            status: 'placed',
+            complete: true,
           },
-        }),
-      ).rejects.toThrowError(ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY));
+        });
+        expect(response.validations).toEqual({
+          input: [],
+          output: [],
+        });
+      });
+
+      // TODO: will be fixed by https://stoplightio.atlassian.net/browse/SO-259
+      xtest('with invalid body returns validation errors', () => {
+        return expect(
+          prism.process({
+            method: 'get',
+            url: {
+              path: '/pet/findByStatus',
+              query: {
+                status: ['available'],
+              },
+            },
+            body: {
+              id: 'should not be a string',
+              status: 'should be one of "placed", "approved", "delivered"',
+              complete: 'should be a boolean',
+            },
+          }),
+        ).rejects.toThrowError(ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY));
+      });
     });
   });
 
