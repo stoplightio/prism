7db547c886e9ea097b9a8c273416340b2f021910
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-06-14 13:50:58 +0200

    feat: fork the process according to the flag

--- packages/cli/src/commands/mock.ts
+++ packages/cli/src/commands/mock.ts
@@ -2,20 +2,21 @@ import { Command } from '@oclif/command';
 import { createLogger, logLevels } from '@stoplight/prism-core';
 import chalk from 'chalk';
 import * as cluster from 'cluster';
-import { LogDescriptor } from 'pino';
+import { LogDescriptor, Logger } from 'pino';
 import * as signale from 'signale';
 import * as split from 'split2';
+import { PassThrough, Readable } from 'stream';
 import { ARGS, FLAGS, LOG_COLOR_MAP } from '../const/options';
 import { createServer } from '../util/createServer';
 
 export default class Server extends Command {
   public static description = 'Start a mock server with the given spec file';
-  public static flags = { port: FLAGS.port, host: FLAGS.host, dynamic: FLAGS.dynamic };
+  public static flags = FLAGS;
   public static args = [ARGS.spec];
 
   public async run() {
     const {
-      flags: { port, dynamic, host },
+      flags: { port, dynamic, host, fork },
       args: { spec },
     } = this.parse(Server);
 
@@ -28,43 +29,63 @@ export default class Server extends Command {
       if (dynamic) {
         signale.star({ prefix: chalk.bgWhiteBright.black('[CLI]'), message: 'Dynamic example generation enabled.' });
       }
+    }
 
-      const worker = cluster.fork();
+    if (fork) {
+      if (cluster.isMaster) {
+        const worker = cluster.fork();
 
-      if (worker.process.stdout) {
-        worker.process.stdout.pipe(split(JSON.parse)).on('data', (logLine: LogDescriptor) => {
-          const logLevelType = logLevels.labels[logLine.level];
-
-          let prefix = LOG_COLOR_MAP[logLine.name].black(`[${logLine.name}]`);
-
-          if (logLine.input) {
-            const { method, url } = logLine.input;
-            prefix = prefix.concat(' ' + chalk.bold.white(`${method} ${url.path}`));
-          }
-
-          signale[logLevelType]({ prefix, message: logLine.msg });
-        });
+        if (worker.process.stdout) {
+          pipeOutputToSignale(worker.process.stdout);
+        }
+      } else {
+        const logInstance = createLogger('CLI');
+        await createPrismServerWithLogger(spec, dynamic, port, host, logInstance);
       }
     } else {
-      const pino = createLogger('CLI');
-      const server = createServer(spec, { mock: { dynamic } });
-      try {
-        const address = await server.listen(port, host);
-
-        if (server.prism.resources.length === 0) {
-          pino.fatal('No operations found in the current file.');
-          cluster.worker.kill();
-        }
-
-        server.prism.resources.forEach(resource => {
-          pino.note(`${resource.method.toUpperCase().padEnd(10)} ${address}${resource.path}`);
-        });
-
-        pino.start(`Prism is listening on ${address}`);
-      } catch (e) {
-        pino.fatal(e.message);
-        cluster.worker.kill();
-      }
+      const logStream = new PassThrough();
+      const logInstance = createLogger('CLI', undefined, logStream);
+      pipeOutputToSignale(logStream);
+      await createPrismServerWithLogger(spec, dynamic, port, host, logInstance);
     }
   }
+}
+
+async function createPrismServerWithLogger(
+  spec: any,
+  dynamic: boolean,
+  port: number,
+  host: string | undefined,
+  logInstance: Logger,
+) {
+  const server = createServer(spec, { mock: { dynamic } });
+  try {
+    const address = await server.listen(port, host);
+    if (server.prism.resources.length === 0) {
+      logInstance.fatal('No operations found in the current file.');
+      cluster.worker.kill();
+    }
+    server.prism.resources.forEach(resource => {
+      logInstance.note(`${resource.method.toUpperCase().padEnd(10)} ${address}${resource.path}`);
+    });
+    logInstance.start(`Prism is listening on ${address}`);
+  } catch (e) {
+    logInstance.fatal(e.message);
+    cluster.worker.kill();
+  }
+}
+
+function pipeOutputToSignale(stream: Readable) {
+  stream.pipe(split(JSON.parse)).on('data', (logLine: LogDescriptor) => {
+    const logLevelType = logLevels.labels[logLine.level];
+
+    let prefix = LOG_COLOR_MAP[logLine.name].black(`[${logLine.name}]`);
+
+    if (logLine.input) {
+      const { method, url } = logLine.input;
+      prefix = prefix.concat(' ' + chalk.bold.white(`${method} ${url.path}`));
+    }
+
+    signale[logLevelType]({ prefix, message: logLine.msg });
+  });
 }
