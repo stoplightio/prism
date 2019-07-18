d90f39be782064190dd34ef6b6346c11782a354e
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-07-18 11:20:05 +0200

    refactor: remove oclif

--- packages/cli/src/commands/mock.ts
+++ packages/cli/src/commands/mock.ts
@@ -1,22 +1,50 @@
-import { Command } from '@oclif/command';
-import { ARGS, FLAGS } from '../const/options';
+import { CommandModule } from 'yargs';
 import { createMultiProcessPrism, createSingleProcessPrism } from '../util/createServer';
 
-export default class Server extends Command {
-  public static description = 'Start a mock server with the given spec file';
-  public static flags = FLAGS;
-  public static args = [ARGS.spec];
+const mockCommand: CommandModule = {
+  describe: 'Start a mock server with the given spec file',
+  command: 'mock <spec>',
+  builder: {
+    port: {
+      alias: 'p',
+      description: 'Port that Prism will run on.',
+      default: 4010,
+      demandOption: true,
+      number: true,
+    },
 
-  public run() {
-    const {
-      flags: { port, dynamic, host, multiprocess },
-      args: { spec },
-    } = this.parse(Server);
+    host: {
+      alias: 'h',
+      description: 'Host that Prism will listen to.',
+      default: '127.0.0.1',
+      demandOption: true,
+      string: true,
+    },
+
+    dynamic: {
+      alias: 'd',
+      description: 'Dynamically generate examples.',
+      boolean: true,
+      default: false,
+    },
+
+    multiprocess: {
+      char: 'm',
+      description: 'Fork the http server from the CLI',
+      boolean: true,
+      default: process.env.NODE_ENV === 'production',
+    },
+  },
+  handler: stuff => {
+    const { multiprocess, dynamic, port, host, spec } = stuff;
 
     if (multiprocess) {
+      // @ts-ignore
       return createMultiProcessPrism({ dynamic, port, host, spec });
     }
-
+    // @ts-ignore
     return createSingleProcessPrism({ dynamic, port, host, spec });
-  }
-}
+  },
+};
+
+module.exports = mockCommand;
