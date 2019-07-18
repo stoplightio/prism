0082c1fa542c041c15655ffdd1ae5e141c5ec05f
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-07-18 11:43:06 +0200

    add positional description

--- packages/cli/src/commands/mock.ts
+++ packages/cli/src/commands/mock.ts
@@ -4,37 +4,43 @@ import { createMultiProcessPrism, createSingleProcessPrism } from '../util/creat
 const mockCommand: CommandModule = {
   describe: 'Start a mock server with the given spec file',
   command: 'mock <spec>',
-  builder: {
-    port: {
-      alias: 'p',
-      description: 'Port that Prism will run on.',
-      default: 4010,
-      demandOption: true,
-      number: true,
-    },
+  builder: yargs =>
+    yargs
+      .positional('spec', {
+        description: 'Path to a spec file. Can be both a file or a fetchable resource on the web',
+        type: 'string',
+      })
+      .options({
+        port: {
+          alias: 'p',
+          description: 'Port that Prism will run on.',
+          default: 4010,
+          demandOption: true,
+          number: true,
+        },
 
-    host: {
-      alias: 'h',
-      description: 'Host that Prism will listen to.',
-      default: '127.0.0.1',
-      demandOption: true,
-      string: true,
-    },
+        host: {
+          alias: 'h',
+          description: 'Host that Prism will listen to.',
+          default: '127.0.0.1',
+          demandOption: true,
+          string: true,
+        },
 
-    dynamic: {
-      alias: 'd',
-      description: 'Dynamically generate examples.',
-      boolean: true,
-      default: false,
-    },
+        dynamic: {
+          alias: 'd',
+          description: 'Dynamically generate examples.',
+          boolean: true,
+          default: false,
+        },
 
-    multiprocess: {
-      char: 'm',
-      description: 'Fork the http server from the CLI',
-      boolean: true,
-      default: process.env.NODE_ENV === 'production',
-    },
-  },
+        multiprocess: {
+          char: 'm',
+          description: 'Fork the http server from the CLI',
+          boolean: true,
+          default: process.env.NODE_ENV === 'production',
+        },
+      }),
   handler: stuff => {
     const { multiprocess, dynamic, port, host, spec } = stuff;
 
