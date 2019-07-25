46d6debda8dee4d357463e60aa36894fbc2816ea
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-07-25 18:15:10 +0200

    refactor: copy servers only if not defined

--- packages/cli/src/util/getHttpOperations.ts
+++ packages/cli/src/util/getHttpOperations.ts
@@ -1,12 +1,7 @@
-import {
-  transformOas2Operation,
-  transformOas2Service,
-  transformOas3Operation,
-  transformOas3Service,
-} from '@stoplight/http-spec';
+import { transformOas2Operation, transformOas3Operation } from '@stoplight/http-spec';
 import { parse } from '@stoplight/yaml';
 import * as fs from 'fs';
-import { flatten, get, keys, map } from 'lodash';
+import { defaults, flatten, get, keys, map } from 'lodash';
 import { httpAndFileResolver } from '../resolvers/http-and-file';
 
 export default async function getHttpOperations(spec: string) {
@@ -16,10 +11,8 @@ export default async function getHttpOperations(spec: string) {
 
   const isOas2 = get(parsedContent, 'swagger');
 
-  const transformServiceFn = isOas2 ? transformOas2Service : transformOas3Service;
   const transformOperationFn = isOas2 ? transformOas2Operation : transformOas3Operation;
 
-  const service = transformServiceFn({ document: resolvedContent });
   const paths = keys(get(resolvedContent, 'paths'));
   const methods = ['get', 'post', 'put', 'delete', 'options', 'head', 'patch', 'trace'];
   return flatten(
@@ -27,13 +20,13 @@ export default async function getHttpOperations(spec: string) {
       keys(get(resolvedContent, ['paths', path]))
         .filter(k => methods.includes(k))
         .map(method =>
-          Object.assign(
+          defaults(
             transformOperationFn({
               document: resolvedContent,
               path,
               method,
             }),
-            service,
+            { servers: resolvedContent.servers },
           ),
         ),
     ),
