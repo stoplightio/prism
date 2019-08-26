3f3162b495c2b5d45026e38c11db05311f2b5a15
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-08-26 15:37:20 -0500

    refactor: getHttpConfigFromRequest should just return what's in the request

--- packages/http-server/src/getHttpConfigFromRequest.ts
+++ packages/http-server/src/getHttpConfigFromRequest.ts
@@ -1,28 +1,18 @@
-import { PartialPrismConfig, PartialPrismConfigFactory, resolveConfig } from '@stoplight/prism-core';
-import { IHttpConfig, IHttpRequest } from '@stoplight/prism-http';
+import { IHttpRequest } from '@stoplight/prism-http';
+import { NegotiatePartialOptions } from 'http/src/mocker/negotiator/types';
 
-export const getHttpConfigFromRequest: PartialPrismConfigFactory<IHttpConfig, IHttpRequest> = (
-  req: IHttpRequest,
-  defaultConfig?: PartialPrismConfig<IHttpConfig, IHttpRequest>,
-) => {
-  // For some reason this fixed the code coverage.
-  let config: Partial<IHttpConfig> = {};
-
-  if (defaultConfig) {
-    config = Object.assign(config, resolveConfig<IHttpConfig, IHttpRequest>(req, defaultConfig));
-  }
-
-  const httpOperationConfig: any = {};
+export const getHttpConfigFromRequest = (req: IHttpRequest): Partial<NegotiatePartialOptions> => {
+  const httpOperationConfig: Partial<NegotiatePartialOptions> = {};
   const query = req.url.query;
 
   if (!query) {
-    return config;
+    return {};
   }
 
   const { __code, __dynamic, __example } = query;
 
   if (__code) {
-    httpOperationConfig.code = __code;
+    httpOperationConfig.code = typeof __code === 'string' ? __code : __code[0];
   }
 
   if (__dynamic) {
@@ -30,17 +20,8 @@ export const getHttpConfigFromRequest: PartialPrismConfigFactory<IHttpConfig, IH
   }
 
   if (__example) {
-    httpOperationConfig.exampleKey = __example;
+    httpOperationConfig.exampleKey = typeof __example === 'string' ? __example : __example[0];
   }
 
-  if (Object.keys(httpOperationConfig).length) {
-    if (typeof config.mock === 'boolean') {
-      config.mock = httpOperationConfig;
-    } else {
-      config.mock = Object.assign({}, config.mock, httpOperationConfig);
-    }
-    return config;
-  }
-
-  return config;
+  return httpOperationConfig;
 };
