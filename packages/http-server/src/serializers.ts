d512d0e9849959f1a564979f8fb3ccc6eb2e2a4f
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-08-28 12:23:00 -0500

    test: fix

--- packages/http-server/src/serializers.ts
+++ packages/http-server/src/serializers.ts
@@ -25,13 +25,12 @@ export default [
       if (typeof data === 'string') {
         return data;
       }
-      throw ProblemJsonError.fromPlainError(
-        Object.assign(new Error('Internal Server Error'), {
-          detail: 'Cannot serialise complex objects as text/plain',
-          status: 500,
-          name: 'https://stoplight.io/prism/errors#NO_COMPLEX_OBJECT_TEXT_PLAIN',
-        }),
-      );
+
+      throw Object.assign(new Error('Cannot serialise complex objects as text/plain'), {
+        detail: 'Cannot serialise complex objects as text/plain',
+        status: 500,
+        name: 'https://stoplight.io/prism/errors#NO_COMPLEX_OBJECT_TEXT_PLAIN',
+      });
     },
   },
 ];
