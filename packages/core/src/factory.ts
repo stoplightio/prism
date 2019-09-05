e0aa4f6548d11d38d71822114a2c1267502475fe
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-09-05 17:19:17 +0200

    refactor: use the proper tools for mapping and providing alternative

--- packages/core/src/factory.ts
+++ packages/core/src/factory.ts
@@ -1,6 +1,6 @@
 import { DiagnosticSeverity } from '@stoplight/types';
 import * as Either from 'fp-ts/lib/Either';
-import { fold } from 'fp-ts/lib/Option';
+import { getOrElse, map } from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
 import * as TaskEither from 'fp-ts/lib/TaskEither';
 import { defaults } from 'lodash';
@@ -56,7 +56,8 @@ export function factory<Resource, Input, Output, Config>(
             const inputValidationResult = inputValidations.concat(
               pipe(
                 validateSecurity(input, resource),
-                fold<IPrismDiagnostic, IPrismDiagnostic[]>(() => [], value => [value]),
+                map(sec => [sec]),
+                getOrElse<IPrismDiagnostic[]>(() => []),
               ),
             );
 
