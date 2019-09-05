5208a13558be0f83c25be529aeb00653648d7cb6
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-09-05 10:43:05 +0200

    wip: http client

---
+++ packages/http/src/__tests__/client.spec.ts
@@ -0,0 +1,7 @@
+import { createInstance } from '../index';
+
+describe('New Http Client tests', () => {
+  test('something', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -1,5 +1,6 @@
-import { factory } from '@stoplight/prism-core';
+import { factory, IPrism } from '@stoplight/prism-core';
 import { IHttpOperation } from '@stoplight/types';
+import { getStatusText } from 'http-status-codes';
 import { defaults } from 'lodash';
 import { forwarder } from './forwarder';
 import { mocker } from './mocker';
@@ -21,9 +22,9 @@ import {
   TPrismHttpInstance,
 } from './types';
 
-const createInstance = (config: IHttpConfig, components?: PickRequired<TPrismHttpComponents, 'logger'>) => {
-  return factory<IHttpOperation, IHttpRequest, IHttpResponse, IHttpConfig>(
-    config,
+const createInstance = (defaultConfig: IHttpConfig, components?: PickRequired<TPrismHttpComponents, 'logger'>) =>
+  factory<IHttpOperation, IHttpRequest, IHttpResponse, IHttpConfig>(
+    defaultConfig,
     defaults(components, {
       router,
       forwarder,
@@ -31,6 +32,76 @@ const createInstance = (config: IHttpConfig, components?: PickRequired<TPrismHtt
       mocker,
     }),
   );
+
+const createNewClientInstance = (
+  defaultConfig: IHttpConfig,
+  components?: PickRequired<TPrismHttpComponents, 'logger'>,
+  defaultResources?: IHttpOperation[],
+): PrismHttp => {
+  const obj = factory<IHttpOperation, IHttpRequest, IHttpResponse, IHttpConfig>(
+    defaultConfig,
+    defaults(components, {
+      router,
+      forwarder,
+      validator,
+      mocker,
+    }),
+  );
+
+  return {
+    request: async (input, resources, config) => {
+      const data = await obj.request(input, resources || defaultResources || [], config);
+
+      if (data.output) {
+        return {
+          status: data.output.statusCode,
+          statusText: getStatusText(data.output.statusCode),
+          headers: data.output.headers || [],
+          data: data.output.body || {},
+          config: defaults(config, defaultConfig),
+          request: input,
+          validations: data.validations,
+        };
+      }
+
+      return data;
+    },
+    get: (input, resources, config) =>
+      obj.request({ method: 'get', ...input }, resources || defaultResources || [], config),
+    put: (input, resources, config) =>
+      obj.request({ method: 'put', ...input }, resources || defaultResources || [], config),
+    post: (input, resources, config) =>
+      obj.request({ method: 'post', ...input }, resources || defaultResources || [], config),
+    delete: (input, resources, config) =>
+      obj.request({ method: 'delete', ...input }, resources || defaultResources || [], config),
+    options: (input, resources, config) =>
+      obj.request({ method: 'options', ...input }, resources || defaultResources || [], config),
+    head: (input, resources, config) =>
+      obj.request({ method: 'head', ...input }, resources || defaultResources || [], config),
+    patch: (input, resources, config) =>
+      obj.request({ method: 'patch', ...input }, resources || defaultResources || [], config),
+    trace: (input, resources, config) =>
+      obj.request({ method: 'trace', ...input }, resources || defaultResources || [], config),
+  };
+};
+
+type requestFn = (
+  input: IHttpRequest,
+  resources?: IHttpOperation[],
+  config?: IHttpConfig,
+) => ReturnType<IPrism<IHttpOperation, IHttpRequest, IHttpResponse, IHttpConfig>['request']>;
+type requestFnVerb = (input: Omit<IHttpRequest, 'method'>, resources?: IHttpOperation[], config?: IHttpConfig) => void;
+
+export type PrismHttp = {
+  request: requestFn;
+  get: requestFnVerb;
+  put: requestFnVerb;
+  post: requestFnVerb;
+  delete: requestFnVerb;
+  options: requestFnVerb;
+  head: requestFnVerb;
+  patch: requestFnVerb;
+  trace: requestFnVerb;
 };
 
 export {
@@ -41,6 +112,7 @@ export {
   IHttpNameValue,
   IHttpNameValues,
   createInstance,
+  createNewClientInstance,
   TPrismHttpInstance,
   IHttpOperationConfig,
   TPrismHttpComponents,
