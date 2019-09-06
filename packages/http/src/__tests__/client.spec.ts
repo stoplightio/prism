4e639ee9a1438be834d16e3405ba323f76cf84fe
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-09-06 11:44:11 +0200

    chore: use nock to not hit the network

--- packages/http/src/__tests__/client.spec.ts
+++ packages/http/src/__tests__/client.spec.ts
@@ -1,7 +1,22 @@
+import * as nock from 'nock';
 import createNewClientInstance from '../client';
 
-describe('New Http Client tests', () => {
+describe('User Http Client', () => {
+  afterEach(() => nock.cleanAll());
+
   test('something', async () => {
+    nock('https://petstore.swagger.io/v2')
+      .get('/pet/10')
+      .reply(200, {
+        license: {
+          key: 'mit',
+          name: 'MIT License',
+          spdx_id: 'MIT',
+          url: 'https://api.github.com/licenses/mit',
+          node_id: 'MDc6TGljZW5zZTEz',
+        },
+      });
+
     const client = await createNewClientInstance(
       { mock: false, validateRequest: true, validateResponse: true },
       require.resolve('../../../../examples/petstore.oas2.yaml'),
