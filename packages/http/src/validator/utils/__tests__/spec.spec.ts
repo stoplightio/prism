a4d26175ed0d852b0ec30428eb0e1660a5889cd8
Karol Maciaszek <karol@maciaszek.pl>
2019-09-25 12:14:35 +0200

    fix: added test, updated changelog

--- packages/http/src/validator/utils/__tests__/spec.spec.ts
+++ packages/http/src/validator/utils/__tests__/spec.spec.ts
@@ -32,6 +32,21 @@ describe('findOperationResponse()', () => {
     });
   });
 
+  describe('when default response is specified with mixed case', () => {
+    it('returns default response', () => {
+      expect(
+        findOperationResponse(
+          [
+            { code: '2XX', contents: [], headers: [] },
+            { code: 'deFAULT', contents: [], headers: [] },
+            { code: '1XX', contents: [], headers: [] },
+          ],
+          422,
+        ),
+      ).toEqual({ code: 'deFAULT', contents: [], headers: [] });
+    });
+  });
+
   describe('when response for given code does not exists and there is no default response', () => {
     it('returns nothing', () => {
       expect(
