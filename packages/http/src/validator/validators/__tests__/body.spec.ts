1c61016652cae8d3c2b8fda24fadb6c00e575b52
Karol Maciaszek <karol@maciaszek.pl>
2019-09-27 10:16:21 +0200

    chore: test, fpism

--- packages/http/src/validator/validators/__tests__/body.spec.ts
+++ packages/http/src/validator/validators/__tests__/body.spec.ts
@@ -1,3 +1,4 @@
+import { HttpParamStyles } from '@stoplight/types/dist';
 import { JSONSchema } from '../../..';
 import { HttpBodyValidator } from '../body';
 
@@ -43,6 +44,56 @@ describe('HttpBodyValidator', () => {
             'application/json',
           ),
         ).toMatchSnapshot();
+      });
+    });
+
+    describe.only('request media type was not found in spec', () => {
+      it('returns no validation errors', () => {
+        expect(
+          httpBodyValidator.validate(
+            'key%5Ba%5D=str&key%5Bb%5D%5Bba%5D=str&key%5Bb%5D%5Bbc%5D%5B0%5D=bc0&key%5Bb%5D%5Bbc%5D%5B1%5D=bc1&key%5Bc%5D%5B0%5D%5Ba%5D=c0a&key%5Bc%5D%5B0%5D%5Bb%5D=c0b&key%5Bc%5D%5B1%5D%5Ba%5D=c0a&key%5Bc%5D%5B1%5D%5Bb%5D=c0b&other=other',
+            [
+              {
+                mediaType: 'application/x-www-form-urlencoded',
+                encodings: [{ property: 'key', style: HttpParamStyles.DeepObject }],
+                schema: {
+                  type: 'object',
+                  properties: {
+                    key: {
+                      type: 'object',
+                      properties: {
+                        a: { type: 'string' },
+                        b: {
+                          type: 'object',
+                          properties: {
+                            ba: { type: 'string' },
+                            bb: { type: 'object' },
+                            bc: { type: 'array' },
+                          },
+                          required: ['ba', 'bb', 'bc'],
+                        },
+                        c: {
+                          type: 'array',
+                          items: {
+                            type: 'object',
+                            properties: {
+                              a: { type: 'string' },
+                              b: { type: 'string' },
+                            },
+                            required: ['a', 'b'],
+                          },
+                        },
+                      },
+                      required: ['a', 'b', 'c'],
+                    },
+                  },
+                  required: ['key'],
+                },
+              },
+            ],
+            'application/x-www-form-urlencoded',
+          ),
+        ).toEqual([]);
       });
     });
   });
--- packages/http/src/validator/validators/body.ts
+++ packages/http/src/validator/validators/body.ts
@@ -5,6 +5,9 @@ import * as typeIs from 'type-is';
 import { body } from '../deserializers';
 
 import { DiagnosticSeverity, Dictionary } from '@stoplight/types';
+import { findFirst } from 'fp-ts/lib/Array';
+import { getOrElse } from 'fp-ts/lib/Option';
+import { pipe } from 'fp-ts/lib/pipeable';
 import { JSONSchema } from '../../types';
 import { validateAgainstSchema } from '../validators/utils';
 import { IHttpValidator } from './types';
@@ -14,8 +17,7 @@ export class HttpBodyValidator implements IHttpValidator<any, IMediaTypeContent>
 
   public validate(target: any, specs: IMediaTypeContent[], mediaType?: string): IPrismDiagnostic[] {
     const { _prefix: prefix } = this;
-    const content = this.getContent(specs, mediaType);
-
+    const content = getContent(specs, mediaType);
     const schema = get(content, 'schema');
 
     if (!schema) {
@@ -23,18 +25,14 @@ export class HttpBodyValidator implements IHttpValidator<any, IMediaTypeContent>
     }
 
     if (mediaType && typeIs.is(mediaType, 'application/x-www-form-urlencoded')) {
-      const shallowParsedTarget = shallowParseUrlEncoded(target);
-      target = Object.entries(shallowParsedTarget).reduce((result, [k, v]) => {
-        result[decodeURIComponent(k)] = decodeURIComponent(v);
-        return result;
-      }, {});
-      const newTarget = {};
+      const uriParams = splitUriParams(target);
+      target = decodeUriEntities(uriParams);
 
       const encodings = get(content, 'encodings', []);
       for (const encoding of encodings) {
         const allowReserved = get(encoding, 'allowReserved', false);
         const property = encoding.property;
-        const value = shallowParsedTarget[property];
+        const value = uriParams[property];
 
         if (!allowReserved && typeof value === 'string' && value.match(/[\/?#\[\]@!$&'()*+,;=]/)) {
           return [
@@ -48,6 +46,8 @@ export class HttpBodyValidator implements IHttpValidator<any, IMediaTypeContent>
       }
 
       if (schema.properties) {
+        const newTarget = {};
+
         for (const property of Object.keys(schema.properties)) {
           newTarget[property] = target[property];
           const encoding = encodings.find(enc => enc.property === property);
@@ -69,26 +69,27 @@ export class HttpBodyValidator implements IHttpValidator<any, IMediaTypeContent>
       Object.assign({}, error, { path: [prefix, ...(error.path || [])] }),
     );
   }
-
-  private getContent(specs: IMediaTypeContent[], mediaType?: string): IMediaTypeContent | undefined {
-    if (!mediaType) {
-      return specs[0];
-    }
-
-    const content = specs.find(c => c.mediaType === mediaType);
-
-    if (!content) {
-      return specs[0];
-    }
-
-    return content;
-  }
 }
 
-function shallowParseUrlEncoded(target: string) {
+function getContent(specs: IMediaTypeContent[], mediaType?: string) {
+  return pipe(
+    specs,
+    findFirst(spec => spec.mediaType === mediaType),
+    getOrElse(() => specs[0]),
+  );
+}
+
+function splitUriParams(target: string) {
   return target.split('&').reduce((result: Dictionary<string, string>, pair: string) => {
     const [key, ...rest] = pair.split('=');
     result[key] = rest.join('=');
+    return result;
+  }, {});
+}
+
+function decodeUriEntities(target: Dictionary<string, string>) {
+  return Object.entries(target).reduce((result, [k, v]) => {
+    result[decodeURIComponent(k)] = decodeURIComponent(v);
     return result;
   }, {});
 }
