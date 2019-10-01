5ca023727508577af6b78db292d032cb7f5421b2
Karol Maciaszek <karol@maciaszek.pl>
2019-10-01 18:20:11 +0200

    test: added failing test

--- packages/http/src/validator/validators/__tests__/body.spec.ts
+++ packages/http/src/validator/validators/__tests__/body.spec.ts
@@ -74,5 +74,39 @@ describe('HttpBodyValidator', () => {
         ).toEqual([]);
       });
     });
+
+    describe('body is form-urlencoded with deep object style and is not compatible with schema', () => {
+      it('returns validation errors', () => {
+        expect(
+          httpBodyValidator.validate(
+            encodeURI('key[a][ab]=str'),
+            [
+              {
+                mediaType: 'application/x-www-form-urlencoded',
+                encodings: [{ property: 'key', style: HttpParamStyles.DeepObject }],
+                schema: {
+                  type: 'object',
+                  properties: {
+                    key: {
+                      type: 'object',
+                      properties: {
+                        a: {
+                          type: 'object',
+                          properties: { aa: { type: 'string' } },
+                          required: ['aa'],
+                        },
+                      },
+                      required: ['a'],
+                    },
+                  },
+                  required: ['key'],
+                },
+              },
+            ],
+            'application/x-www-form-urlencoded',
+          ),
+        ).toMatchSnapshot();
+      });
+    });
   });
 });
