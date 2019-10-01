40daa6e9ab27941f7a64d221acc52caa978cc292
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-10-01 12:00:43 +0200

    Apply suggestions from code review
    Co-Authored-By: Karol Maciaszek <karol@maciaszek.pl>

--- packages/http-server/src/server.ts
+++ packages/http-server/src/server.ts
@@ -71,7 +71,7 @@ export const createServer = (operations: IHttpOperation[], opts: IPrismHttpServe
     request.log.info({ input }, 'Request received');
     try {
       const operationSpecificConfig = getHttpConfigFromRequest(input);
-      const mockConfig = Object.assign({}, opts.config.mock, operationSpecificConfig);
+      const mockConfig = { ...opts.config.mock, ...operationSpecificConfig };
 
       const response = await prism.request(input, operations, {
         ...opts.config,
