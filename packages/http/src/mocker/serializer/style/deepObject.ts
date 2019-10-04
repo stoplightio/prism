76fbbd874503d79ccbf61be8e9d6c1a5dbe09449
Karol Maciaszek <karol@maciaszek.pl>
2019-10-04 10:51:31 +0200

    fix: validating array index is positive integer

--- packages/http/src/mocker/serializer/style/deepObject.ts
+++ packages/http/src/mocker/serializer/style/deepObject.ts
@@ -7,13 +7,13 @@ export function serializeWithDeepObjectStyle(name: string, value: any) {
 function serialize(name: string, path: string[], value: any): string {
   if (typeof value === 'object') {
     return Object.keys(value)
-      .map(key => serialize(name, [...path, isNormalInteger(key) ? '' : key], value[key]))
+      .map(key => serialize(name, [...path, isPositiveInteger(key) ? '' : key], value[key]))
       .join('&');
   } else {
     return `${name}${path.map(key => `[${key}]`).join('')}=${value}`;
   }
 }
 
-function isNormalInteger(str: string) {
-  return /^\+?\d+$/.test(str);
+function isPositiveInteger(str: string) {
+  return /^\+?\d+$/.test(str) && parseInt(str) >= 0;
 }
