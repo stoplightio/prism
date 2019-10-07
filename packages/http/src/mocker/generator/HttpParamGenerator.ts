966ba3a902d47106b19f4bfa5142c62d80454f7d
Karol Maciaszek <karol@maciaszek.pl>
2019-10-07 18:16:29 +0200

    feat: improve quality of generated param examples

--- packages/http/src/mocker/generator/HttpParamGenerator.ts
+++ packages/http/src/mocker/generator/HttpParamGenerator.ts
@@ -3,9 +3,45 @@ import { INodeExample, INodeExternalExample } from '@stoplight/types';
 import * as Array from 'fp-ts/lib/Array';
 import * as Option from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
+import { JSONSchema } from '../../types';
 import { generate as generateDynamicExample } from './JSONSchema';
 
-function generateStaticExample(examples: Array<INodeExample | INodeExternalExample>) {
+function improveSchema(schema: JSONSchema) {
+  const newSchema = { ...schema };
+
+  if (newSchema.type === 'integer') {
+    if (!newSchema.minimum) {
+      newSchema.minimum = 1;
+    }
+
+    if (!newSchema.maximum) {
+      newSchema.maximum = 1000;
+    }
+  }
+
+  if (newSchema.type === 'string') {
+    // @todo: use faker to generate single-worded string
+  }
+
+  if (newSchema.type === 'object') {
+    if (newSchema.properties) {
+      newSchema.properties = Object.entries(newSchema.properties).reduce((r, [k, v]) => {
+        r[k] = improveSchema(v);
+        return r;
+      }, {});
+    }
+  }
+
+  if (newSchema.type === 'array') {
+    if (typeof newSchema.items === 'object') {
+      newSchema.items = improveSchema(newSchema.items);
+    }
+  }
+
+  return newSchema;
+}
+
+function generateStaticExample(examples: Array<INodeExample | INodeExternalExample>): Option.Option<unknown> {
   return pipe(
     Option.fromNullable(examples),
     Option.mapNullable(exs => exs[Math.floor(Math.random() * exs.length)]),
@@ -20,6 +56,7 @@ export function generate(param: IHttpParam): Option.Option<unknown> {
     Option.alt(() =>
       pipe(
         Option.fromNullable(param.schema),
+        Option.map(improveSchema),
         Option.map(generateDynamicExample),
       ),
     ),
