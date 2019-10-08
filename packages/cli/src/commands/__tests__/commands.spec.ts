6277a0b42079cc7b5d9d6ded74a72b1667a08504
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-10-08 18:15:44 +0200

    test: expand for proxy command as well

--- packages/cli/src/commands/__tests__/commands.spec.ts
+++ packages/cli/src/commands/__tests__/commands.spec.ts
@@ -2,8 +2,9 @@ import * as utils from '@stoplight/prism-http';
 import * as yargs from 'yargs';
 import { createMultiProcessPrism, createSingleProcessPrism } from '../../util/createServer';
 import mockCommand from '../mock';
+import proxyCommand from '../proxy';
 
-const parser = yargs.command(mockCommand);
+const parser = yargs.command(mockCommand).command(proxyCommand);
 
 jest.mock('../../util/createServer', () => ({
   createMultiProcessPrism: jest.fn(),
@@ -12,7 +13,10 @@ jest.mock('../../util/createServer', () => ({
 
 jest.spyOn(utils, 'getHttpOperationsFromResource').mockResolvedValue([]);
 
-describe.each([['mock'], ['proxy', 'http://github.com']])('%s command', (command, upstream) => {
+describe.each<{ 0: string; 1: string; 2: unknown }>([
+  ['mock', '', { dynamic: false }],
+  ['proxy', 'http://github.com', { upstream: new URL('http://github.com/') }],
+])('%s command', (command, upstream, additionalCallOptions) => {
   beforeEach(() => {
     (createSingleProcessPrism as jest.Mock).mockClear();
     (createMultiProcessPrism as jest.Mock).mockClear();
@@ -28,12 +32,12 @@ describe.each([['mock'], ['proxy', 'http://github.com']])('%s command', (command
     expect(createMultiProcessPrism).not.toHaveBeenCalled();
     expect(createSingleProcessPrism).toHaveBeenLastCalledWith({
       operations: [],
-      dynamic: false,
       multiprocess: false,
       log: 'stdout',
       cors: true,
       host: '127.0.0.1',
       port: 4010,
+      ...additionalCallOptions,
     });
   });
 
@@ -47,12 +51,12 @@ describe.each([['mock'], ['proxy', 'http://github.com']])('%s command', (command
     expect(createMultiProcessPrism).not.toHaveBeenCalled();
     expect(createSingleProcessPrism).toHaveBeenLastCalledWith({
       operations: [],
-      dynamic: false,
       multiprocess: false,
       log: 'stdout',
       cors: true,
       host: '127.0.0.1',
       port: 666,
+      ...additionalCallOptions,
     });
   });
 
@@ -66,12 +70,12 @@ describe.each([['mock'], ['proxy', 'http://github.com']])('%s command', (command
     expect(createMultiProcessPrism).not.toHaveBeenCalled();
     expect(createSingleProcessPrism).toHaveBeenLastCalledWith({
       operations: [],
-      dynamic: false,
       multiprocess: false,
       log: 'stdout',
       cors: true,
       host: '0.0.0.0',
       port: 4010,
+      ...additionalCallOptions,
     });
   });
 
@@ -88,10 +92,10 @@ describe.each([['mock'], ['proxy', 'http://github.com']])('%s command', (command
       operations: [],
       cors: true,
       log: 'stdout',
-      dynamic: false,
       multiprocess: false,
       host: '0.0.0.0',
       port: 666,
+      ...additionalCallOptions,
     });
   });
 
@@ -105,12 +109,12 @@ describe.each([['mock'], ['proxy', 'http://github.com']])('%s command', (command
     expect(createSingleProcessPrism).not.toHaveBeenCalled();
     expect(createMultiProcessPrism).toHaveBeenLastCalledWith({
       operations: [],
-      dynamic: false,
       cors: true,
       log: 'stdout',
       multiprocess: true,
       host: '0.0.0.0',
       port: 4010,
+      ...additionalCallOptions,
     });
   });
 
@@ -125,12 +129,12 @@ describe.each([['mock'], ['proxy', 'http://github.com']])('%s command', (command
     expect(createSingleProcessPrism).not.toHaveBeenCalled();
     expect(createMultiProcessPrism).toHaveBeenLastCalledWith({
       operations: [],
-      dynamic: false,
       cors: true,
       log: 'httpResponse',
       multiprocess: true,
       host: '0.0.0.0',
       port: 4010,
+      ...additionalCallOptions,
     });
   });
 });
