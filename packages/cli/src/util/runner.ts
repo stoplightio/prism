bde81b625afb94046085872030b6fc061fe3fab0
Mateusz Wit <mateuszwit21@gmail.com>
2019-10-11 11:34:31 +0200

    refactor: some ts weirdness adjustment

--- packages/cli/src/util/runner.ts
+++ packages/cli/src/util/runner.ts
@@ -6,33 +6,39 @@ import { CreatePrismOptions } from './createServer';
 type CreatePrism = (options: CreatePrismOptions) => Promise<IPrismHttpServer | undefined>;
 
 export function runPrismAndSetupWatcher(createPrism: CreatePrism, options: CreatePrismOptions, spec: string) {
-  return createPrism(options).then(server => {
-    if (server) {
+  return createPrism(options).then(possiblyServer => {
+    if (possiblyServer) {
+      let server: IPrismHttpServer = possiblyServer;
+
       const watcher = chokidar.watch(spec, { awaitWriteFinish: { stabilityThreshold: 500, pollInterval: 100 } });
 
       watcher.on('change', () => {
-        server!.fastify.log.info('Restarting Prism...');
+        server.fastify.log.info('Restarting Prism...');
 
         getHttpOperationsFromResource(spec)
           .then(operations => {
             if (operations.length === 0) {
-              server!.fastify.log.info(
+              server.fastify.log.info(
                 'No operations found in the current file, continuing with the previously loaded spec.',
               );
             } else {
-              return server!.fastify.close()
+              return server.fastify.close()
                 .then(() => {
-                  server!.fastify.log.info('Loading the updated operations...');
+                  server.fastify.log.info('Loading the updated operations...');
 
                   return createPrism({ ...options, operations });
                 })
-                .then((newServer) => server = newServer);
+                .then((newServer) => {
+                  if (newServer) {
+                    server = newServer;
+                  }
+                });
             }
           })
           .catch(() => {
-            server!.fastify.log.info('Something went terribly wrong, trying to start Prism with the original document.');
+            server.fastify.log.info('Something went terribly wrong, trying to start Prism with the original document.');
 
-            return server!.fastify
+            return server.fastify
               .close()
               .then(() => createPrism(options))
               .catch(() => process.exit(1))
