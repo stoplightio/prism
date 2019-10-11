859d7dee48c69351a3f8a4e3f4a4a952ec7d29ed
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-10-11 11:31:53 +0200

    feat: replace with node-fetch

--- packages/http/src/forwarder/index.ts
+++ packages/http/src/forwarder/index.ts
@@ -1,32 +1,31 @@
 import { IPrismComponents } from '@stoplight/prism-core';
 import { IHttpOperation } from '@stoplight/types';
-import axios from 'axios';
+import { fromPairs } from 'lodash';
+import fetch from 'node-fetch';
 import { toError } from 'fp-ts/lib/Either';
 import * as TaskEither from 'fp-ts/lib/TaskEither';
 import { defaults, omit } from 'lodash';
+import { format } from 'url';
 import { IHttpConfig, IHttpRequest, IHttpResponse } from '../types';
+
 const { version: prismVersion } = require('../../package.json');
 
 const forward: IPrismComponents<IHttpOperation, IHttpRequest, IHttpResponse, IHttpConfig>['forward'] = (
   input: IHttpRequest,
-  baseUrl: string,
+  baseUrl: string
 ): TaskEither.TaskEither<Error, IHttpResponse> =>
-  TaskEither.tryCatch<Error, IHttpResponse>(async () => {
-    const response = await axios.request<unknown>({
-      method: input.method as any,
-      baseURL: baseUrl,
-      url: input.url.path,
-      params: input.url.query,
-      data: input.body,
-      headers: defaults(omit(input.headers, 'host'), { 'user-agent': `Prism/${prismVersion}` }),
-      validateStatus: () => true,
-    });
-
-    return {
-      statusCode: response.status,
-      headers: response.headers,
-      body: response.data,
-    };
-  }, toError);
+  TaskEither.tryCatch<Error, IHttpResponse>(
+    () =>
+      fetch(format({ hostname: baseUrl, pathname: input.url.path, query: input.url.query }), {
+        headers: defaults(omit(input.headers, 'host'), { 'user-agent': `Prism/${prismVersion}` }),
+      })
+        .then(r => Promise.all([r, r.json()]))
+        .then<IHttpResponse>(([response, body]) => ({
+          statusCode: response.status,
+          headers: fromPairs(Object.entries(response.headers)),
+          body,
+        })),
+    toError
+  );
 
 export default forward;
