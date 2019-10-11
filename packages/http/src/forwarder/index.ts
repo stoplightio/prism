0299127e7ed3abc14d84b32338dbc9ea9b483dd9
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-10-11 11:33:03 +0200

    chore: move prettier rc out

--- packages/http/src/forwarder/index.ts
+++ packages/http/src/forwarder/index.ts
@@ -1,12 +1,14 @@
 import { IPrismComponents } from '@stoplight/prism-core';
-import { IHttpOperation } from '@stoplight/types';
-import { fromPairs } from 'lodash';
+import { IHttpOperation, Dictionary } from '@stoplight/types';
+import { mapValues } from 'lodash';
 import fetch from 'node-fetch';
+import * as typeIs from 'type-is';
 import { toError } from 'fp-ts/lib/Either';
 import * as TaskEither from 'fp-ts/lib/TaskEither';
 import { defaults, omit } from 'lodash';
-import { format } from 'url';
+import { format, parse, resolve } from 'url';
 import { IHttpConfig, IHttpRequest, IHttpResponse } from '../types';
+import { posix } from 'path';
 
 const { version: prismVersion } = require('../../package.json');
 
@@ -14,18 +16,35 @@ const forward: IPrismComponents<IHttpOperation, IHttpRequest, IHttpResponse, IHt
   input: IHttpRequest,
   baseUrl: string
 ): TaskEither.TaskEither<Error, IHttpResponse> =>
-  TaskEither.tryCatch<Error, IHttpResponse>(
-    () =>
-      fetch(format({ hostname: baseUrl, pathname: input.url.path, query: input.url.query }), {
-        headers: defaults(omit(input.headers, 'host'), { 'user-agent': `Prism/${prismVersion}` }),
-      })
-        .then(r => Promise.all([r, r.json()]))
-        .then<IHttpResponse>(([response, body]) => ({
-          statusCode: response.status,
-          headers: fromPairs(Object.entries(response.headers)),
-          body,
-        })),
-    toError
-  );
+  TaskEither.tryCatch<Error, IHttpResponse>(() => {
+    const partialUrl = parse(baseUrl);
+    return fetch(
+      format({
+        protocol: partialUrl.protocol,
+        hostname: partialUrl.hostname,
+        pathname: posix.join(partialUrl.pathname || '', input.url.path),
+        query: input.url.query,
+      }),
+      {
+        headers: defaults(omit(input.headers, ['host', 'accept']), {
+          accept: 'application/json, text/plain, */*',
+          'user-agent': `Prism/${prismVersion}`,
+        }),
+      }
+    )
+      .then(r =>
+        Promise.all([
+          r,
+          typeIs.is(r.headers.get('content-type') || '', ['application/json', 'application/*+json'])
+            ? r.json()
+            : r.text(),
+        ])
+      )
+      .then<IHttpResponse>(([response, body]) => ({
+        statusCode: response.status,
+        headers: mapValues(response.headers.raw(), hValue => hValue.join(' ')),
+        body,
+      }));
+  }, toError);
 
 export default forward;
