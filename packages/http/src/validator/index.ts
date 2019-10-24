58407713b2375d91832dfe6d28c41c32d3280d3a
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-10-24 16:00:42 +0200

    chore: format

--- packages/http/src/validator/index.ts
+++ packages/http/src/validator/index.ts
@@ -3,7 +3,7 @@ import { DiagnosticSeverity, IHttpOperation, IHttpOperationResponse } from '@sto
 import * as caseless from 'caseless';
 import { findFirst } from 'fp-ts/lib/Array';
 import * as Option from 'fp-ts/lib/Option';
-import * as Either from 'fp-ts/lib/Either'
+import * as Either from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { inRange } from 'lodash';
 import { IHttpRequest, IHttpResponse } from '../types';
@@ -13,11 +13,9 @@ import { HttpBodyValidator, HttpHeadersValidator, HttpQueryValidator } from './v
 import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
 import { sequenceValidation, sequenceOption } from './validators/utils';
 
-
 export const bodyValidator = new HttpBodyValidator('body');
 export const headersValidator = new HttpHeadersValidator(headerDeserializerRegistry, 'header');
 export const queryValidator = new HttpQueryValidator(queryDeserializerRegistry, 'query');
-
 
 const validateInput: ValidatorFn<IHttpOperation, IHttpRequest> = ({ resource, element }) => {
   const mediaType = caseless(element.headers || {}).get('content-type');
@@ -27,54 +25,58 @@ const validateInput: ValidatorFn<IHttpOperation, IHttpRequest> = ({ resource, el
   const validateBody = pipe(
     Option.fromNullable(request),
     Option.mapNullable(request => request.body),
-    Option.chain(requestBody => pipe(
-      requestBody,
-      Option.fromPredicate(requestBody => !!requestBody.required && !body),
-      Option.map<unknown, NonEmptyArray<IPrismDiagnostic>>(
-        () => [{ code: 'required', message: 'Body parameter is required', severity: DiagnosticSeverity.Error }]
-      ),
-      Option.alt(() =>
-        pipe(
-          sequenceOption(Option.fromNullable(body), Option.fromNullable(requestBody.contents)),
-          Option.chain(([body, contents]) =>
-            pipe(
-              bodyValidator.validate(body, contents, mediaType),
-              Either.swap,
-              Option.fromEither
+    Option.chain(requestBody =>
+      pipe(
+        requestBody,
+        Option.fromPredicate(requestBody => !!requestBody.required && !body),
+        Option.map<unknown, NonEmptyArray<IPrismDiagnostic>>(() => [
+          { code: 'required', message: 'Body parameter is required', severity: DiagnosticSeverity.Error },
+        ]),
+        Option.alt(() =>
+          pipe(
+            sequenceOption(Option.fromNullable(body), Option.fromNullable(requestBody.contents)),
+            Option.chain(([body, contents]) =>
+              pipe(
+                bodyValidator.validate(body, contents, mediaType),
+                Either.swap,
+                Option.fromEither
+              )
             )
           )
         )
       )
-    )),
-    Either.fromOption(() => { }),
+    ),
+    Either.fromOption(() => {}),
     Either.swap
-  )
+  );
 
   return sequenceValidation(
     validateBody,
     headersValidator.validate(element.headers || {}, (request && request.headers) || []),
     queryValidator.validate(element.url.query || {}, (request && request.query) || [])
-  )
+  );
 };
 
-const findResponseByStatus = (responses: NonEmptyArray<IHttpOperationResponse>, statusCode: number) => pipe(
-  findOperationResponse(responses, statusCode),
-  Either.fromOption<IPrismDiagnostic>(() => ({
-    message: 'Unable to match the returned status code with those defined in spec',
-    severity: inRange(statusCode, 200, 300) ? DiagnosticSeverity.Error : DiagnosticSeverity.Warning,
-  })),
-  Either.mapLeft<IPrismDiagnostic, NonEmptyArray<IPrismDiagnostic>>(error => [error])
-);
+const findResponseByStatus = (responses: NonEmptyArray<IHttpOperationResponse>, statusCode: number) =>
+  pipe(
+    findOperationResponse(responses, statusCode),
+    Either.fromOption<IPrismDiagnostic>(() => ({
+      message: 'Unable to match the returned status code with those defined in spec',
+      severity: inRange(statusCode, 200, 300) ? DiagnosticSeverity.Error : DiagnosticSeverity.Warning,
+    })),
+    Either.mapLeft<IPrismDiagnostic, NonEmptyArray<IPrismDiagnostic>>(error => [error])
+  );
 
-const mismatchMediaType = (response: IHttpOperationResponse, mediaType: string) => pipe(
-  Option.fromNullable(response.contents),
-  Option.chain(findFirst(c => c.mediaType === mediaType)),
-  Either.fromOption<IPrismDiagnostic>(() => ({
-    message: `The received media type does not match the one specified in the document`,
-    severity: DiagnosticSeverity.Error,
-  })),
-  Either.mapLeft<IPrismDiagnostic, NonEmptyArray<IPrismDiagnostic>>(e => [e])
-);
+const mismatchMediaType = (response: IHttpOperationResponse, mediaType: string) =>
+  pipe(
+    Option.fromNullable(response.contents),
+    Option.chain(findFirst(c => c.mediaType === mediaType)),
+    Either.fromOption<IPrismDiagnostic>(() => ({
+      message: `The received media type does not match the one specified in the document`,
+      severity: DiagnosticSeverity.Error,
+    })),
+    Either.mapLeft<IPrismDiagnostic, NonEmptyArray<IPrismDiagnostic>>(e => [e])
+  );
 
 const validateOutput: ValidatorFn<IHttpOperation, IHttpResponse> = ({ resource, element }) => {
   const mediaType = caseless(element.headers || {}).get('content-type');
@@ -84,10 +86,10 @@ const validateOutput: ValidatorFn<IHttpOperation, IHttpResponse> = ({ resource, 
       sequenceValidation(
         mismatchMediaType(response, mediaType),
         bodyValidator.validate(element.body, response.contents || [], mediaType),
-        headersValidator.validate(element.headers || {}, response.headers || []),
+        headersValidator.validate(element.headers || {}, response.headers || [])
       )
     )
-  )
+  );
 };
 
 export { validateInput, validateOutput };
