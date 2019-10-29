d082edef340c1bb7853484ecdb942a17ae4d4bd0
Mateusz Wit <mateuszwit21@gmail.com>
2019-10-29 10:54:07 +0100

    refactor: just use unknown

--- packages/cli/src/util/colorizer.ts
+++ packages/cli/src/util/colorizer.ts
@@ -1,8 +1,7 @@
 import { mapValues, isArray } from 'lodash';
 import { Dictionary } from '@stoplight/types';
 
-type ParamValue = number | string | string[] | unknown;
-export type ValuesTransformer = (values: Dictionary<ParamValue>) => Dictionary<string | string[]>;
+export type ValuesTransformer = (values: Dictionary<unknown>) => Dictionary<string | string[]>;
 
 export const PRE_PARAM_VALUE_TAG = 'PRE_PARAM_VALUE_TAG';
 export const POST_PARAM_VALUE_TAG = 'POST_PARAM_VALUE_TAG';
@@ -17,6 +16,6 @@ export const attachTagsToParamsValues: ValuesTransformer = values => {
   return mapValues(values, attachPrePostTags);
 };
 
-const attachPrePostTags = (paramValue: ParamValue) => {
+const attachPrePostTags = (paramValue: unknown) => {
   return [PRE_PARAM_VALUE_TAG].concat(isArray(paramValue) ? paramValue : `${paramValue}`).concat(POST_PARAM_VALUE_TAG);
 };
