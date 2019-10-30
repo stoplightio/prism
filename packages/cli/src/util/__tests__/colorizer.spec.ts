ad2f0dbd1239035834895afba0503f329fa9d978
Mateusz Wit <mateuszwit21@gmail.com>
2019-10-30 17:06:31 +0100

    test: adjust tests

--- packages/cli/src/util/__tests__/colorizer.spec.ts
+++ packages/cli/src/util/__tests__/colorizer.spec.ts
@@ -46,21 +46,21 @@ describe('colorizer', () => {
   describe('transformPathParamsValues()', () => {
     it('colorizes tagged values of query params', () => {
       const paramVal = 'sold,pending';
-      const path = `/no_auth/pets/findByStatus?status=${PRE_PARAM_VALUE_TAG},${paramVal},${POST_PARAM_VALUE_TAG}`;
+      const path = `/no_auth/pets/findByStatus?status=${PRE_PARAM_VALUE_TAG}${paramVal}${POST_PARAM_VALUE_TAG}`;
 
       expect(transformPathParamsValues(path, chalk.bold.blue)).toBe(`/no_auth/pets/findByStatus?status=${chalk.bold.blue(paramVal)}`);
     });
 
     it('colorizes tagged values of path params', () => {
       const paramVal = 651;
-      const path = `/no_auth/pets/${PRE_PARAM_VALUE_TAG},${paramVal},${POST_PARAM_VALUE_TAG}`;
+      const path = `/no_auth/pets/${PRE_PARAM_VALUE_TAG}${paramVal}${POST_PARAM_VALUE_TAG}`;
 
       expect(transformPathParamsValues(path, chalk.bold.blue)).toBe(`/no_auth/pets/${chalk.bold.blue(`${paramVal}`)}`);
     });
 
     it('colorizes values that are equal to multiple or single , characters', () => {
       const paramVal = ',,,,,,,,,,,,,';
-      const path = `/no_auth/pets/${PRE_PARAM_VALUE_TAG},${paramVal},${POST_PARAM_VALUE_TAG}`;
+      const path = `/no_auth/pets/${PRE_PARAM_VALUE_TAG}${paramVal}${POST_PARAM_VALUE_TAG}`;
 
       expect(transformPathParamsValues(path, chalk.bold.blue)).toBe(`/no_auth/pets/${chalk.bold.blue(paramVal)}`);
     });
@@ -80,14 +80,12 @@ describe('colorizer', () => {
 
         expect(attachTagsToParamsValues(values)).toStrictEqual({
           status: [
-            PRE_PARAM_VALUE_TAG,
-            'available',
-            'pending',
-            'sold',
-            POST_PARAM_VALUE_TAG
+            `${PRE_PARAM_VALUE_TAG}available${POST_PARAM_VALUE_TAG}`,
+            `${PRE_PARAM_VALUE_TAG}pending${POST_PARAM_VALUE_TAG}`,
+            `${PRE_PARAM_VALUE_TAG}sold${POST_PARAM_VALUE_TAG}`
           ]
         });
-      });
+      })
 
       describe('tagging single values', () => {
         it('tags string values', () => {
@@ -97,11 +95,7 @@ describe('colorizer', () => {
           };
 
           expect(attachTagsToParamsValues(valuesOfParams)).toStrictEqual({
-            name: [
-              PRE_PARAM_VALUE_TAG,
-              val,
-              POST_PARAM_VALUE_TAG
-            ]
+            name: `${PRE_PARAM_VALUE_TAG}${val}${POST_PARAM_VALUE_TAG}`,
           });
         });
 
@@ -112,11 +106,7 @@ describe('colorizer', () => {
           };
 
           expect(attachTagsToParamsValues(valuesOfParams)).toStrictEqual({
-            petId: [
-              PRE_PARAM_VALUE_TAG,
-              `${val}`,
-              POST_PARAM_VALUE_TAG
-            ]
+            petId: `${PRE_PARAM_VALUE_TAG}${val}${POST_PARAM_VALUE_TAG}`
           });
         });
 
@@ -127,11 +117,7 @@ describe('colorizer', () => {
           };
 
           expect(attachTagsToParamsValues(valuesOfParams)).toStrictEqual({
-            name: [
-              PRE_PARAM_VALUE_TAG,
-              val,
-              POST_PARAM_VALUE_TAG
-            ]
+            name: `${PRE_PARAM_VALUE_TAG}${val}${POST_PARAM_VALUE_TAG}`
           });
         });
       });
