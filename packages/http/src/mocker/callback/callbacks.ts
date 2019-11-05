30cb0bec167b8f1a7a9a830facc386a2c4c21cf4
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-11-05 17:46:55 -0600

    refactor: unknown error

--- packages/http/src/mocker/callback/callbacks.ts
+++ packages/http/src/mocker/callback/callbacks.ts
@@ -1,12 +1,12 @@
 import { IHttpCallbackOperation, IHttpOperationRequest } from '@stoplight/types';
 import { resolveRuntimeExpressions } from '../../utils/runtimeExpression';
 import { IHttpRequest, IHttpResponse } from '../../types';
-import fetch  from 'node-fetch';
+import fetch from 'node-fetch';
 import * as Option from 'fp-ts/lib/Option';
 import * as Either from 'fp-ts/lib/Either';
 import { map, reduce } from 'fp-ts/lib/Array';
-import * as Reader from 'fp-ts/lib/Reader';
 import * as TaskEither from 'fp-ts/lib/TaskEither';
+import * as ReaderTaskEither from 'fp-ts/lib/ReaderTaskEither';
 import { head } from 'fp-ts/lib/Array';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { generate as generateHttpParam } from '../generator/HttpParamGenerator';
@@ -16,47 +16,66 @@ import withLogger from '../../withLogger';
 import { violationLogger } from '../../utils/logger';
 import { Logger } from 'pino';
 
-export function runCallback({ callback, request, response }: { callback: IHttpCallbackOperation, request: IHttpRequest, response: IHttpResponse }): Reader.Reader<Logger, TaskEither.TaskEither<Error, unknown>> {
+export function runCallback({
+  callback,
+  request,
+  response,
+}: {
+  callback: IHttpCallbackOperation;
+  request: IHttpRequest;
+  response: IHttpResponse;
+}): ReaderTaskEither.ReaderTaskEither<Logger, void, unknown> {
   return withLogger(logger => {
-    const { url, ...requestInit } = assembleRequest({ resource: callback, request, response });
+    const { url, requestData } = assembleRequest({ resource: callback, request, response });
     const logViolation = violationLogger(logger);
 
     logger.info({ name: 'CALLBACK' }, `${callback.callbackName}: Making request to ${url}...`);
 
     return pipe(
-      TaskEither.tryCatch(() => fetch(url, requestInit), Either.toError),
+      TaskEither.tryCatch(() => fetch(url, requestData), Either.toError),
       TaskEither.chain(parseResponse),
-      TaskEither.map(element => {
+      TaskEither.mapLeft(error =>
+        logger.error({ name: 'CALLBACK' }, `${callback.callbackName}: Request failed: ${error.message}`)
+      ),
+      TaskEither.chain(element => {
         logger.info({ name: 'CALLBACK' }, `${callback.callbackName}: Request finished`);
 
         return pipe(
           validateOutput({ resource: callback, element }),
           Either.mapLeft(violations => {
-            pipe(violations, map(logViolation));
-            return violations;
+            pipe(
+              violations,
+              map(logViolation)
+            );
           }),
-          Either.alt(() => Either.right(element)),
+          TaskEither.fromEither
         );
-      }),
-      TaskEither.mapLeft(error => {
-        logger.error({ name: 'CALLBACK' }, `${callback.callbackName}: Request failed: ${error.message}`);
-        return error;
-      }),
+      })
     );
   });
 }
 
-function assembleRequest({ resource, request, response }: { resource: IHttpCallbackOperation, request: IHttpRequest, response: IHttpResponse }) {
+function assembleRequest({
+  resource,
+  request,
+  response,
+}: {
+  resource: IHttpCallbackOperation;
+  request: IHttpRequest;
+  response: IHttpResponse;
+}) {
   const bodyAndMediaType = Option.toUndefined(assembleBody(resource.request));
   return {
     url: resolveRuntimeExpressions(resource.path, request, response),
-    headers: Option.toUndefined(assembleHeaders(resource.request, bodyAndMediaType && bodyAndMediaType.mediaType)),
-    body: bodyAndMediaType && bodyAndMediaType.body,
-        method: resource.method,
-};
+    requestData: {
+      headers: Option.toUndefined(assembleHeaders(resource.request, bodyAndMediaType && bodyAndMediaType.mediaType)),
+      body: bodyAndMediaType && bodyAndMediaType.body,
+      method: resource.method,
+    },
+  };
 }
 
-function assembleBody(request?: IHttpOperationRequest): Option.Option<{ body: string, mediaType: string }> {
+function assembleBody(request?: IHttpOperationRequest): Option.Option<{ body: string; mediaType: string }> {
   return pipe(
     Option.fromNullable(request),
     Option.mapNullable(request => request.body),
@@ -65,33 +84,43 @@ function assembleBody(request?: IHttpOperationRequest): Option.Option<{ body: st
     Option.chain(param => {
       return pipe(
         generateHttpParam(param),
-        Option.chain(body => pipe(
-          Either.stringifyJSON(body, () => undefined),
-          Option.fromEither,
-        )),
-        Option.map(body => ({ body, mediaType: param.mediaType })),
+        Option.chain(body =>
+          pipe(
+            Either.stringifyJSON(body, () => undefined),
+            Option.fromEither
+          )
+        ),
+        Option.map(body => ({ body, mediaType: param.mediaType }))
       );
-    }),
+    })
   );
 }
 
-function assembleHeaders(request?: IHttpOperationRequest, bodyMediaType?: string): Option.Option<{ [key: string]: string }> {
+function assembleHeaders(
+  request?: IHttpOperationRequest,
+  bodyMediaType?: string
+): Option.Option<{ [key: string]: string }> {
   return pipe(
     Option.fromNullable(request),
     Option.mapNullable(request => request.headers),
-    Option.map(params => pipe(
-      params,
-      reduce({}, (headers, param) => pipe(
-        param,
-        generateHttpParam,
-        Option.fold(() => headers, value => ({ ...headers, [param.name]: value }))
-      )),
-    )),
+    Option.map(params =>
+      pipe(
+        params,
+        reduce({}, (headers, param) =>
+          pipe(
+            param,
+            generateHttpParam,
+            Option.fold(() => headers, value => ({ ...headers, [param.name]: value }))
+          )
+        )
+      )
+    ),
     Option.reduce(
       pipe(
         Option.fromNullable(bodyMediaType),
-        Option.map((mediaType) => ({ 'content-type': mediaType })),
+        Option.map(mediaType => ({ 'content-type': mediaType }))
       ),
-      (mediaTypeHeader, headers) => ({ ...headers, ...mediaTypeHeader })),
+      (mediaTypeHeader, headers) => ({ ...headers, ...mediaTypeHeader })
+    )
   );
 }
