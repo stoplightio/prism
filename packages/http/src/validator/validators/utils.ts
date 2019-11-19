db3b978407f4db3b8399821a696411e78d2c1aa3
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-11-19 12:37:35 +0100

    fix: use bae to improve messages

--- packages/http/src/validator/validators/utils.ts
+++ packages/http/src/validator/validators/utils.ts
@@ -4,14 +4,14 @@ import { getSemigroup } from 'fp-ts/lib/NonEmptyArray';
 import { getValidation } from 'fp-ts/lib/Either';
 import { option } from 'fp-ts/lib/Option';
 import { sequenceT } from 'fp-ts/lib/Apply';
-import * as Ajv from 'ajv';
+import { ErrorObject } from 'ajv';
+import * as betterAjvErrors from 'better-ajv-errors';
 import { JSONSchema } from '../../';
-// @ts-ignore
 import * as AjvOAI from 'ajv-oai';
 
-const ajv = new AjvOAI({ allErrors: true, messages: true, schemaId: 'auto' }) as Ajv.Ajv;
+const ajv = new AjvOAI({ allErrors: true, messages: true, schemaId: 'auto', jsonPointers: true });
 
-export const convertAjvErrors = (errors: Ajv.ErrorObject[] | undefined | null, severity: DiagnosticSeverity) => {
+export const convertAjvErrors = (errors: ErrorObject[] | undefined | null, severity: DiagnosticSeverity) => {
   if (!errors) {
     return [];
   }
@@ -29,9 +29,10 @@ export const validateAgainstSchema = (value: any, schema: JSONSchema, prefix?: s
     const validate = ajv.compile(schema);
     const valid = validate(value);
     if (!valid) {
-      return convertAjvErrors(validate.errors, DiagnosticSeverity.Error).map(error => {
+      const enhacedMessages = betterAjvErrors(schema, value, validate.errors, { format: 'js' });
+      return convertAjvErrors(validate.errors, DiagnosticSeverity.Error).map((error, index) => {
         const path = prefix ? [prefix, ...error.path] : [...error.path];
-        return Object.assign({}, error, { path });
+        return Object.assign({}, error, { path, message: enhacedMessages[index].error });
       });
     }
     return [];
