398e70fcbbf0fc323f4f99c9efe43a793dffe82a
Vincenzo Chianese <vincenz.chianese@icloud.com>
2019-12-01 09:47:57 +0100

    refactor: make the function more clear

--- packages/http/src/validator/validators/utils.ts
+++ packages/http/src/validator/validators/utils.ts
@@ -1,6 +1,6 @@
 import { IPrismDiagnostic } from '@stoplight/prism-core';
 import { DiagnosticSeverity, Segment } from '@stoplight/types';
-import { getSemigroup, NonEmptyArray } from 'fp-ts/lib/NonEmptyArray';
+import { getSemigroup, NonEmptyArray, fromArray, map } from 'fp-ts/lib/NonEmptyArray';
 import { getValidation } from 'fp-ts/lib/Either';
 import * as Option from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
@@ -11,23 +11,20 @@ import * as AjvOAI from 'ajv-oai';
 
 const ajv = new AjvOAI({ allErrors: true, messages: true, schemaId: 'auto' });
 
-export const convertAjvErrors = (errors: Ajv.ErrorObject[] | undefined | null, severity: DiagnosticSeverity) => {
-  if (!errors) {
-    return [];
-  }
+export const convertAjvErrors = (errors: NonEmptyArray<Ajv.ErrorObject>, severity: DiagnosticSeverity) =>
+  pipe(
+    errors,
+    map(error => {
+      const allowedParameters = 'allowedValues' in error.params ? `: ${error.params.allowedValues.join(', ')}` : '';
 
-  return errors.map<IPrismDiagnostic & { path: Segment[] }>(error => {
-    const allowedParameters = 'allowedValues' in error.params ? `: ${error.params.allowedValues.join(', ')}` : '';
-
-    return {
-      path: error.dataPath.split('.').slice(1),
-      code: error.keyword || '',
-      message: `${error.message || ''}${allowedParameters}`,
-      severity,
-    };
-  });
-};
-
+      return {
+        path: error.dataPath.split('.').slice(1),
+        code: error.keyword || '',
+        message: `${error.message || ''}${allowedParameters}`,
+        severity,
+      };
+    })
+  );
 export const validateAgainstSchema = (
   value: unknown,
   schema: JSONSchema,
@@ -37,16 +34,18 @@ export const validateAgainstSchema = (
     Option.tryCatch(() => ajv.compile(schema)),
     Option.chain(validate => {
       const valid = validate(value);
-      if (!valid) {
-        return Option.some(
-          convertAjvErrors(validate.errors, DiagnosticSeverity.Error).map(error => {
-            const path = prefix ? [prefix, ...error.path] : error.path;
-            return Object.assign({}, error, { path });
-          }) as NonEmptyArray<IPrismDiagnostic>
-        );
-      }
+      if (!valid) return fromArray(validate.errors!);
       return Option.none;
-    })
+    }),
+    Option.map(errors =>
+      pipe(
+        convertAjvErrors(errors, DiagnosticSeverity.Error),
+        map(error => {
+          const path = prefix ? [prefix, ...error.path] : error.path;
+          return Object.assign({}, error, { path });
+        })
+      )
+    )
   );
 };
 
