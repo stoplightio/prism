8aacd777de0360e5afc7d1ea921b5d687e107d6e
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-02-13 17:56:41 +0100

    rewrite the function to use the new codec

--- packages/http-server/src/getHttpConfigFromRequest.ts
+++ packages/http-server/src/getHttpConfigFromRequest.ts
@@ -1,26 +1,26 @@
-import { IHttpOperationConfig, IHttpRequest } from '@stoplight/prism-http';
+import { IHttpOperationConfig, IHttpRequest, ProblemJsonError, UNPROCESSABLE_ENTITY } from '@stoplight/prism-http';
+import { pipe } from 'fp-ts/lib/pipeable';
+import * as E from 'fp-ts/lib/Either';
+import * as t from 'io-ts';
+import { failure } from 'io-ts/lib/PathReporter';
+import { BooleanFromString } from 'io-ts-types/lib/BooleanFromString';
 
-export const getHttpConfigFromRequest = (req: IHttpRequest): Partial<IHttpOperationConfig> => {
-  const httpOperationConfig: Partial<IHttpOperationConfig> = {};
-  const query = req.url.query;
+const preferencesDecoder = t.partial(
+  {
+    __code: t.string,
+    __dynamic: t.string.pipe(BooleanFromString),
+    __example: t.string,
+  },
+  'Preferences'
+);
 
-  if (!query) {
-    return {};
-  }
-
-  const { __code, __dynamic, __example } = query;
-
-  if (__code) {
-    httpOperationConfig.code = typeof __code === 'string' ? __code : __code[0];
-  }
-
-  if (__dynamic) {
-    httpOperationConfig.dynamic = __dynamic === 'true';
-  }
-
-  if (__example) {
-    httpOperationConfig.exampleKey = typeof __example === 'string' ? __example : __example[0];
-  }
-
-  return httpOperationConfig;
-};
+export const getHttpConfigFromRequest = (
+  req: IHttpRequest
+): E.Either<Error, Partial<Omit<IHttpOperationConfig, 'mediaType'>>> =>
+  pipe(
+    preferencesDecoder.decode(req.url.query),
+    E.bimap(
+      err => ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY, failure(err).join('; ')),
+      parsed => ({ code: parsed.__code, dynamic: parsed.__dynamic, exampleKey: parsed.__example })
+    )
+  );
