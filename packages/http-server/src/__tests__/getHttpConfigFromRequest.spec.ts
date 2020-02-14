64c547e69dcfe3b16869fdc6bb034d3c6cc63985
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-02-14 11:17:52 +0100

    test: add header tests

--- packages/http-server/src/__tests__/getHttpConfigFromRequest.spec.ts
+++ packages/http-server/src/__tests__/getHttpConfigFromRequest.spec.ts
@@ -3,54 +3,102 @@ import { assertRight } from '@stoplight/prism-core/src/__tests__/utils';
 
 describe('getHttpConfigFromRequest()', () => {
   describe('given no default config', () => {
-    test('and no query should return my own default', () => {
-      return assertRight(
-        getHttpConfigFromRequest({
-          method: 'get',
-          url: { path: '/' },
-        }),
-        parsed => expect(parsed).toEqual({})
-      );
+    describe('query', () => {
+      test('and no query should return my own default', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/' },
+          }),
+          parsed => expect(parsed).toEqual({})
+        );
+      });
+
+      test('and no matching query should return my own default', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/', query: {} },
+          }),
+          parsed => expect(parsed).toEqual({})
+        );
+      });
+
+      test('extracts code', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/', query: { __code: '202' } },
+          }),
+          parsed => expect(parsed).toHaveProperty('code', '202')
+        );
+      });
+
+      test('extracts example', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/', query: { __example: 'bear' } },
+          }),
+          parsed => expect(parsed).toHaveProperty('exampleKey', 'bear')
+        );
+      });
+
+      test('extracts dynamic', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/', query: { __dynamic: 'true' } },
+          }),
+          parsed => expect(parsed).toHaveProperty('dynamic', true)
+        );
+      });
     });
 
-    test('and no matching query should return my own default', () => {
-      return assertRight(
-        getHttpConfigFromRequest({
-          method: 'get',
-          url: { path: '/', query: {} },
-        }),
-        parsed => expect(parsed).toEqual({})
-      );
-    });
+    describe.only('headers', () => {
+      test('extracts code', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/' },
+            headers: { prefer: '__code=202' },
+          }),
+          parsed => expect(parsed).toHaveProperty('code', '202')
+        );
+      });
 
-    test('extracts code', () => {
-      return assertRight(
-        getHttpConfigFromRequest({
-          method: 'get',
-          url: { path: '/', query: { __code: '202' } },
-        }),
-        parsed => expect(parsed).toHaveProperty('code', '202')
-      );
-    });
+      test('extracts example', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/' },
+            headers: { prefer: '__example=bear' },
+          }),
+          parsed => expect(parsed).toHaveProperty('exampleKey', 'bear')
+        );
+      });
 
-    test('extracts example', () => {
-      return assertRight(
-        getHttpConfigFromRequest({
-          method: 'get',
-          url: { path: '/', query: { __example: 'bear' } },
-        }),
-        parsed => expect(parsed).toHaveProperty('exampleKey', 'bear')
-      );
-    });
+      test('extracts dynamic', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/' },
+            headers: { prefer: '__dynamic=true' },
+          }),
+          parsed => expect(parsed).toHaveProperty('dynamic', true)
+        );
+      });
 
-    test('extracts dynamic', () => {
-      return assertRight(
-        getHttpConfigFromRequest({
-          method: 'get',
-          url: { path: '/', query: { __dynamic: 'true' } },
-        }),
-        parsed => expect(parsed).toHaveProperty('dynamic', true)
-      );
+      test('prefers header over query', () => {
+        return assertRight(
+          getHttpConfigFromRequest({
+            method: 'get',
+            url: { path: '/', query: { __dynamic: 'false' } },
+            headers: { prefer: '__dynamic=true' },
+          }),
+          parsed => expect(parsed).toHaveProperty('dynamic', true)
+        );
+      });
     });
   });
 });
