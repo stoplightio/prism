95e390e278719638b7d7aaaab0af98de34baf2e7
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-02-14 11:44:25 +0100

    refactor: simplify

--- packages/http-server/src/getHttpConfigFromRequest.ts
+++ packages/http-server/src/getHttpConfigFromRequest.ts
@@ -7,7 +7,7 @@ import { BooleanFromString } from 'io-ts-types/lib/BooleanFromString';
 //@ts-ignore
 import * as parsePreferHeader from 'parse-prefer-header';
 
-const headerPreferencesDecoder = t.union([
+const PreferencesDecoder = t.union([
   t.undefined,
   t.partial(
     {
@@ -15,38 +15,23 @@ const headerPreferencesDecoder = t.union([
       dynamic: t.string.pipe(BooleanFromString),
       example: t.string,
     },
-    'headerPreferences'
-  ),
-]);
-
-const queryPreferencesDecoder = t.union([
-  t.undefined,
-  t.partial(
-    {
-      __code: t.string,
-      __dynamic: t.string.pipe(BooleanFromString),
-      __example: t.string,
-    },
-    'queryPreferences'
+    'Preferences'
   ),
 ]);
 
 type requestPreference = Partial<Omit<IHttpOperationConfig, 'mediaType'>>;
 
 export const getHttpConfigFromRequest = (req: IHttpRequest): E.Either<Error, requestPreference> => {
-  const preferenceSource =
+  const preferences =
     req.headers && req.headers['prefer']
-      ? pipe(
-          headerPreferencesDecoder.decode(parsePreferHeader(req.headers['prefer'])),
-          E.map(parsed => ({ code: parsed?.code, dynamic: parsed?.dynamic, exampleKey: parsed?.example }))
-        )
-      : pipe(
-          queryPreferencesDecoder.decode(req.url.query),
-          E.map(parsed => ({ code: parsed?.__code, dynamic: parsed?.__dynamic, exampleKey: parsed?.__example }))
-        );
+      ? parsePreferHeader(req.headers['prefer'])
+      : { code: req.url.query?.__code, dynamic: req.url.query?.__dynamic, example: req.url.query?.__example };
 
   return pipe(
-    preferenceSource,
-    E.mapLeft(err => ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY, failure(err).join('; ')))
+    PreferencesDecoder.decode(preferences),
+    E.bimap(
+      err => ProblemJsonError.fromTemplate(UNPROCESSABLE_ENTITY, failure(err).join('; ')),
+      parsed => ({ code: parsed?.code, exampleKey: parsed?.example, dynamic: parsed?.dynamic })
+    )
   );
 };
