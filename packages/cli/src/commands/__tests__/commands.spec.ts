133696c1ba7406ee95f501519862309d0785b345
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-05-07 12:51:00 -0500

    fix import

--- packages/cli/src/commands/__tests__/commands.spec.ts
+++ packages/cli/src/commands/__tests__/commands.spec.ts
@@ -1,4 +1,4 @@
-import * as operationUtils from '@stoplight/prism-cli/src/operations';
+import * as operationUtils from '../../operations';
 import * as yargs from 'yargs';
 import { createMultiProcessPrism, createSingleProcessPrism } from '../../util/createServer';
 import mockCommand from '../mock';
