283fec22911a4b3e35f625df00fd77abbbedd5a7
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-09-17 18:15:02 -0500

    remove type not used

--- packages/http/src/validator/deserializers/types.ts
+++ packages/http/src/validator/deserializers/types.ts
@@ -1,21 +1,3 @@
-import { HttpParamStyles } from '@stoplight/types';
-import { IHttpNameValue, IHttpNameValues, JSONSchema } from '../../types';
+import { JSONSchema } from '../../types';
 
-export type deserializeFn<Parameters> = (
-  name: string,
-  parameters: Parameters,
-  schema?: JSONSchema,
-  explode?: boolean
-) => unknown;
-
-export interface IHttpParamDeserializerRegistry<Parameters, S = HttpParamStyles> {
-  get(style: S): IHttpParamStyleDeserializer<Parameters> | undefined;
-}
-
-export interface IHttpParamStyleDeserializer<Parameters, S = HttpParamStyles> {
-  supports: (style: S) => boolean;
-  deserialize: deserializeFn<Parameters>;
-}
-
-export type IHttpHeaderParamStyleDeserializer = IHttpParamStyleDeserializer<IHttpNameValue>;
-export type IHttpQueryParamStyleDeserializer = IHttpParamStyleDeserializer<IHttpNameValues>;
+export type deserializeFn<T> = (name: string, parameters: T, schema?: JSONSchema, explode?: boolean) => unknown;
