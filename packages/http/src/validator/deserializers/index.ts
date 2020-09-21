af94037ccc2345af572cc99b8919db2519735823
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-09-21 17:24:55 -0500

    be precise with types

--- packages/http/src/validator/deserializers/index.ts
+++ packages/http/src/validator/deserializers/index.ts
@@ -4,14 +4,20 @@ import deepObject from './style/deepObject';
 import label from './style/label';
 import matrix from './style/matrix';
 import delimited from './style/delimited';
+import type { deserializeFn } from './types';
+import type { IHttpNameValue, IHttpNameValues } from '../../index';
+import type { Dictionary } from '@stoplight/types';
 
-export const header = { simple };
-export const query = {
+export const header: Dictionary<deserializeFn<IHttpNameValue>, 'simple'> = { simple };
+export const query: Dictionary<
+  deserializeFn<IHttpNameValues>,
+  'form' | 'spaceDelimited' | 'pipeDelimited' | 'commaDelimited' | 'deepObject'
+> = {
   form,
   spaceDelimited: delimited('%20'),
   pipeDelimited: delimited('|'),
   commaDelimited: delimited(','),
   deepObject,
 };
-export const path = { simple, label, matrix };
+export const path: Dictionary<deserializeFn<IHttpNameValue>, 'simple' | 'label' | 'matrix'> = { simple, label, matrix };
 export const body = query;
