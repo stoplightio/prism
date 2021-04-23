11f01ab9fe06a92e2a54926428154c9684dcd667
Jakub Rożek <jakub@stoplight.io>
2021-04-23 12:16:41 +0200

    chore: tweak ajv options

--- packages/http/src/validator/validators/utils.ts
+++ packages/http/src/validator/validators/utils.ts
@@ -3,17 +3,23 @@ import { DiagnosticSeverity } from '@stoplight/types';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { NonEmptyArray, fromArray, map } from 'fp-ts/NonEmptyArray';
-import type { ErrorObject } from 'ajv';
-import Ajv from 'ajv';
+import Ajv, { ErrorObject, Options } from 'ajv';
 import type AjvCore from 'ajv/dist/core';
 import Ajv2019 from 'ajv/dist/2019';
 import Ajv2020 from 'ajv/dist/2020';
 import addFormats from 'ajv-formats';
 import type { JSONSchema } from '../../';
 
+const baseAjvOptions: Partial<Options> = {
+  allErrors: true,
+  allowUnionTypes: true,
+  allowMatchingProperties: true,
+  strict: false,
+};
+
 function createAjvInstances(Ajv: typeof AjvCore) {
-  const ajv = new Ajv({ allErrors: true, messages: true, coerceTypes: true, strict: false });
-  const ajvNoCoerce = new Ajv({ allErrors: true, messages: true, coerceTypes: false, strict: false });
+  const ajv = new Ajv({ ...baseAjvOptions, coerceTypes: true });
+  const ajvNoCoerce = new Ajv({ ...baseAjvOptions, coerceTypes: false });
 
   addFormats(ajv);
   addFormats(ajvNoCoerce);
