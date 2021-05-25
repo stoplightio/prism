08d1f60cca33c63c8df889550dff2f12a500a83f
Thomas Pytleski <pytlesk4@gmail.com>
2021-05-25 08:59:35 -0500

    chore: update stoplight json

--- packages/cli/src/operations.ts
+++ packages/cli/src/operations.ts
@@ -2,9 +2,9 @@ import { transformOas3Operations } from '@stoplight/http-spec/oas3/operation';
 import { transformOas2Operations } from '@stoplight/http-spec/oas2/operation';
 import { transformPostmanCollectionOperations } from '@stoplight/http-spec/postman/operation';
 import { dereference } from 'json-schema-ref-parser';
-import { bundleTarget, pathToPointer } from '@stoplight/json';
+import { bundleTarget, decycle } from '@stoplight/json';
 import { IHttpOperation } from '@stoplight/types';
-import { get, isPlainObject } from 'lodash';
+import { get } from 'lodash';
 import type { Spec } from 'swagger-schema-official';
 import type { OpenAPIObject } from 'openapi3-ts';
 import type { CollectionDefinition } from 'postman-collection';
@@ -43,38 +43,3 @@ function isOpenAPI3(document: unknown): document is OpenAPIObject {
 function isPostmanCollection(document: unknown): document is CollectionDefinition {
   return Array.isArray(get(document, 'item')) && get(document, 'info.name');
 }
-
-// TODO: remove, need https://github.com/stoplightio/json/pull/90
-export const decycle = (obj: unknown, replacer?: (value: any) => any) => {
-  const objs = new WeakMap<object, string>();
-  return (function derez(value: any, path: string[]) {
-    // The new object or array
-    let curObj: any;
-
-    // If a replacer function was provided, then call it to get a replacement value.
-    if (replacer) value = replacer(value);
-
-    if (isPlainObject(value) || Array.isArray(value)) {
-      // The path of an earlier occurance of value
-      const oldPath = objs.get(value);
-      // If the value is an object or array, look to see if we have already
-      // encountered it. If so, return a {"$ref":PATH} object.
-      if (oldPath) return { $ref: oldPath };
-
-      objs.set(value, pathToPointer(path));
-      // If it is an array, replicate the array.
-      if (Array.isArray(value)) {
-        curObj = value.map((element, i) => derez(element, [...path, String(i)]));
-      } else {
-        // It is an object, replicate the object.
-        curObj = {};
-        Object.keys(value).forEach(name => {
-          curObj[name] = derez(value[name], [...path, name]);
-        });
-      }
-      objs.delete(value);
-      return curObj;
-    }
-    return value;
-  })(obj, []);
-};
