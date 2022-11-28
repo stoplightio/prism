54c94364d8e0088ae05a8f1326a76b66c1650cad
Daniel A. White <daniel.white@stoplight.io>
2022-11-28 12:06:48 -0500

    improved x-json-schema-faker options handling

--- packages/http/src/mocker/generator/JSONSchema.ts
+++ packages/http/src/mocker/generator/JSONSchema.ts
@@ -2,7 +2,7 @@ import faker from '@faker-js/faker';
 import { cloneDeep } from 'lodash';
 import { JSONSchema } from '../../types';
 
-import * as jsf from 'json-schema-faker';
+import { JSONSchemaFaker, JSONSchemaFakerOptions } from 'json-schema-faker';
 import * as sampler from '@stoplight/json-schema-sampler';
 import { Either, toError, tryCatch } from 'fp-ts/Either';
 import { IHttpOperation } from '@stoplight/types';
@@ -10,22 +10,52 @@ import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/lib/Either';
 import { stripWriteOnlyProperties } from '../../utils/filterRequiredProperties';
 
-// necessary as workaround broken types in json-schema-faker
-// @ts-ignore
-jsf.extend('faker', () => faker);
+JSONSchemaFaker.extend('faker', () => faker);
 
-// necessary as workaround broken types in json-schema-faker
-// @ts-ignore
-jsf.option({
-  failOnInvalidTypes: false,
-  failOnInvalidFormat: false,
-  alwaysFakeOptionals: true,
-  optionalsProbability: 1 as any,
-  fixedProbabilities: true,
-  ignoreMissingRefs: true,
-  maxItems: 20,
-  maxLength: 100,
-});
+// From https://github.com/json-schema-faker/json-schema-faker/tree/develop/docs
+// Using from entries since the types aren't 100% compatible
+const JSON_SCHEMA_FAKER_DEFAULT_OPTIONS: Readonly<JSONSchemaFakerOptions> = Object.fromEntries([
+  ['defaultInvalidTypeProduct', null],
+  ['defaultRandExpMax', 10],
+  ['pruneProperties', []],
+  ['ignoreProperties', []],
+  ['ignoreMissingRefs', false],
+  ['failOnInvalidTypes', true],
+  ['failOnInvalidFormat', true],
+  ['alwaysFakeOptionals', false],
+  ['optionalsProbability', false],
+  ['fixedProbabilities', false],
+  ['useExamplesValue', false],
+  ['useDefaultValue', false],
+  ['requiredOnly', false],
+  ['minItems', 0],
+  ['maxItems', null],
+  ['minLength', 0],
+  ['maxLength', null],
+  ['refDepthMin', 0],
+  ['refDepthMax', 3],
+  ['resolveJsonPath', false],
+  ['reuseProperties', false],
+  ['sortProperties', null],
+  ['fillProperties', true],
+  ['random', Math.random],
+  ['replaceEmptyByRandomValue', false],
+  ['omitNulls', false],
+]);
+
+export function resetGenerator() {
+  JSONSchemaFaker.option({
+    ...JSON_SCHEMA_FAKER_DEFAULT_OPTIONS,
+    failOnInvalidTypes: false,
+    failOnInvalidFormat: false,
+    alwaysFakeOptionals: true,
+    optionalsProbability: 1,
+    fixedProbabilities: true,
+    ignoreMissingRefs: true,
+  });
+}
+
+resetGenerator();
 
 export function generate(bundle: unknown, source: JSONSchema): Either<Error, unknown> {
   return pipe(
@@ -33,9 +63,7 @@ export function generate(bundle: unknown, source: JSONSchema): Either<Error, unk
     E.fromOption(() => Error('Cannot strip writeOnly properties')),
     E.chain(updatedSource =>
       tryCatch(
-        // necessary as workaround broken types in json-schema-faker
-        // @ts-ignore
-        () => sortSchemaAlphabetically(jsf.generate({ ...cloneDeep(updatedSource), __bundled__: bundle })),
+        () => sortSchemaAlphabetically(JSONSchemaFaker.generate({ ...cloneDeep(updatedSource), __bundled__: bundle })),
         toError
       )
     )
